dcd3d5d6b55d4d5b675d5aed8b141231b1e0c4c4
David Stevenson <drstevenson1958@yahoo.com>
2023-08-01 09:13:48 -0400

    Listing 3-12. The Contents of the Repository.cs File in the Models Folder

---
+++ ch03/PartyInvites/Models/Repository.cs
@@ -0,0 +1,15 @@
+﻿namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new();
+
+        public static IEnumerable<GuestResponse> Responses => responses;
+
+        public static void AddResponse(GuestResponse response)
+        {
+            Console.WriteLine(responses);
+            responses.Add(response);
+        }
+    }
+}
