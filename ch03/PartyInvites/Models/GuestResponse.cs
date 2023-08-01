2f634cd8087d992e5c00415bd4475671003962fa
David Stevenson <drstevenson1958@yahoo.com>
2023-08-01 10:18:29 -0400

    Listing 3-17. Applying Validation in the GuestResponse.cs File in the Models Folder

--- ch03/PartyInvites/Models/GuestResponse.cs
+++ ch03/PartyInvites/Models/GuestResponse.cs
@@ -1,10 +1,21 @@
-﻿namespace PartyInvites.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [Phone]
         public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
         public bool? WillAttend { get; set; }
     }
 }
