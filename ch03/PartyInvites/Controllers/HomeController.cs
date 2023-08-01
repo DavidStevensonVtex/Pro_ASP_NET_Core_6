6de64b80bc187ae0501bf73977473db6ac754173
David Stevenson <drstevenson1958@yahoo.com>
2023-08-01 10:22:56 -0400

    Listing 3-18. Checking for Validation Errors in the HOmeController.cs File in the Controllers Folder

--- ch03/PartyInvites/Controllers/HomeController.cs
+++ ch03/PartyInvites/Controllers/HomeController.cs
@@ -19,8 +19,15 @@ namespace PartyInvites.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            Repository.AddResponse(guestResponse);
-            return View("Thanks", guestResponse);
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public ViewResult ListResponses()
