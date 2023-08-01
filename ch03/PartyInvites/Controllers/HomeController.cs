7526cb4511763651bc1cd438e2f0d3154ebc0952
David Stevenson <drstevenson1958@yahoo.com>
2023-08-01 09:13:12 -0400

    Listing 3-11. ADding a Method in the HomeController.cs File in the Controllers Folder

--- ch03/PartyInvites/Controllers/HomeController.cs
+++ ch03/PartyInvites/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -8,9 +9,17 @@ namespace PartyInvites.Controllers
         {
             return View();
         }
-        
+
+        [HttpGet]
         public ViewResult RsvpForm()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: store response from the guest
             return View();
         }
     }
