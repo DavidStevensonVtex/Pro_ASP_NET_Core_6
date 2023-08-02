626b50a740ab09f9da66015697995c3b635d53cb
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 08:39:17 -0400

    Listing 5-21. Using a Collection Initializer in the HomeController.cs File in the Controllers Folder

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -4,12 +4,7 @@
     {
         public ViewResult Index()
         {
-            string[] names = new string[3];
-            names[0] = "Bob";
-            names[1] = "Joe";
-            names[2] = "Alice";
-
-            return View(names);
+            return View(new string[] { "Bob", "Joe", "Alice" });
         }
     }
 }
