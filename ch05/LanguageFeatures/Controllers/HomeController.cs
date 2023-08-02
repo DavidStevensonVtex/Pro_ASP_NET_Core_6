bf524c3df87b63aac011544ec2307408a8b1ef58
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 10:18:40 -0400

    Listing 5-48. Using an Interface in the HomeController.cs File in the Controllers Folder

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -4,14 +4,14 @@
     {
         public ViewResult Index()
         {
-            var products = new[]
-            {
-                new { Name = "Kayak", Price = 275M },
-                new { Name = "Lifejacket", Price = 48.95M },
-                new { Name = "Soccer ball", Price = 19.50M },
-                new { Name = "Corner flag", Price = 34.95M }
-            };
-            return View(products.Select(p => p.GetType().Name));
+            IProductSelection cart = new ShoppingCart(
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M },
+                new Product { Name = "Soccer ball", Price = 19.50M },
+                new Product { Name = "Corner flag", Price = 34.95M }
+            );
+
+            return View(cart.Products?.Select(p => p.Name));
         }
     }
 }
