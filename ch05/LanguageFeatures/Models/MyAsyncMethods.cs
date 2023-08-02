7dcb83925a791f842c8cc3528bd1c4d0c7a5fc7f
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 10:27:44 -0400

    Listing 5-51. The Contents of the MyAsyncMethods.cs File in the Models Folder

---
+++ ch05/LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace LanguageFeatures.Models
+{
+    public class MyAsyncMethods
+    {
+        public static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+            var httpTask = client.GetAsync("http://apress.com");
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
+            {
+                return antecedent.Result.Content.Headers.ContentLength;
+            });
+        }
+    }
+}
