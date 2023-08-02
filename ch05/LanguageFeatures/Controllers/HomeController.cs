348d8a12efaefdfe97002ebcc6f3f4ea869823f9
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 08:46:49 -0400

    Listing 5-23. Using Collection Initializer Syntax in the HomeController.cs File in the Controllers Folder

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -6,8 +6,8 @@
         {
             Dictionary<string, Product> products = new Dictionary<string, Product>
             {
-                { "Kayak", new Product { Name = "Kayak", Price = 275M } },
-                { "Lifejacket", new Product { Name = "Lifejacket", Price = 48.95M } }
+                ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
+                ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
             };
 
             return View(products.Keys);
