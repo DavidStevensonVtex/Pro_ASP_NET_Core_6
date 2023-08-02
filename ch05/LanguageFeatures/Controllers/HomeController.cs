a58cc5c66585a7c52610ff97d30aeb6d5d08b7ca
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 10:57:08 -0400

    Listing 5-57. Using an Asynchronous Enumerable in the HomeController.cs File in the Controllers Folder

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -5,7 +5,7 @@
         public async Task<ViewResult> Index()
         {
             List<string> output = new List<string>();
-            foreach (long? len in await MyAsyncMethods.GetPageLengths(output,
+            await foreach (long? len in MyAsyncMethods.GetPageLengths(output,
                 "apress.com", "microsoft.com", "amazon.com"))
             {
                 output.Add($"Page length: {len}");
