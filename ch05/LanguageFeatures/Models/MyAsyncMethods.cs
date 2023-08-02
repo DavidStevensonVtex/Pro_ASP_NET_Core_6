4e4059d2c8705041f4318be47a8e03b1ad5da3d4
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 10:54:49 -0400

    Listing 5-56. Using an Asynchronous Enumerable in the MyAsyncMethods.cs File in the Models Folder

--- ch05/LanguageFeatures/Models/MyAsyncMethods.cs
+++ ch05/LanguageFeatures/Models/MyAsyncMethods.cs
@@ -11,20 +11,16 @@ namespace LanguageFeatures.Models
             return httpMessage.Content.Headers.ContentLength;
         }
 
-        public static async Task<IEnumerable<long?>> GetPageLengths(
-            List<string> output, params string[] urls)
+        public static async IAsyncEnumerable<long?> GetPageLengths(List<string> output, params string[] urls)
         {
-            List<long?> results = new List<long?>();
             HttpClient client = new HttpClient();
             foreach (string url in urls)
             {
                 output.Add($"Started request for {url}");
                 var httpMessage = await client.GetAsync($"http://{url}");
-                results.Add(httpMessage.Content.Headers.ContentLength);
                 output.Add($"Completed request for {url}");
+                yield return httpMessage.Content.Headers.ContentLength;
             }
-
-            return results;
         }
 
     }
