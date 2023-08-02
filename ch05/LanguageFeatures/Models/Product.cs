2d2c9bf86fcfc530c9c26d340667c0de73c39754
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 08:15:54 -0400

    Listing 5-12. Using a Nullable Type in the Product.cs File in the Models Folder

--- ch05/LanguageFeatures/Models/Product.cs
+++ ch05/LanguageFeatures/Models/Product.cs
@@ -6,12 +6,12 @@
 
         public decimal? Price { get; set; }
 
-        public static Product[] GetProducts()
+        public static Product?[] GetProducts()
         {
             Product kayak = new Product { Name = "Kayak", Price = 275M };
             Product lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
 
-            return new Product[] { kayak, lifejacket, null };
+            return new Product?[] { kayak, lifejacket, null };
         }
     }
 }
