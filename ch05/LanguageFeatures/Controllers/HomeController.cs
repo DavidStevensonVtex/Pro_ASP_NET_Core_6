311945383ef4023dc54caa424495ead2caf144cb
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 10:47:22 -0400

    Listing 5-54. Adding a Method in the MyAsyncMethods.cs File in the Models Folder

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -2,26 +2,10 @@
 {
     public class HomeController : Controller
     {
-        public async Task<ViewResult> GetPageLength()
+        public async Task<ViewResult> Index()
         {
             long? length = await MyAsyncMethods.GetPageLength();
             return View(new string[] { $"Length: {length}" });
-        }
-
-        public static async Task<IEnumerable<long?>> GetPageLengths(
-            List<string>output, params string[] urls)
-        {
-            List<long?> results = new List<long?>();
-            HttpClient client = new HttpClient();
-            foreach (string url in urls)
-            {
-                output.Add($"Started request for {url}");
-                var httpMessage = await client.GetAsync($"http://{url}");
-                results.Add(httpMessage.Content.Headers.ContentLength);
-                output.Add($"Completed request for {url}");
-            }
-
-            return results;
         }
     }
 }
--- ch05/LanguageFeatures/Models/MyAsyncMethods.cs
+++ ch05/LanguageFeatures/Models/MyAsyncMethods.cs
@@ -10,5 +10,22 @@ namespace LanguageFeatures.Models
             var httpMessage = await client.GetAsync("http://apress.com");
             return httpMessage.Content.Headers.ContentLength;
         }
+
+        public static async Task<IEnumerable<long?>> GetPageLengths(
+            List<string> output, params string[] urls)
+        {
+            List<long?> results = new List<long?>();
+            HttpClient client = new HttpClient();
+            foreach (string url in urls)
+            {
+                output.Add($"Started request for {url}");
+                var httpMessage = await client.GetAsync($"http://{url}");
+                results.Add(httpMessage.Content.Headers.ContentLength);
+                output.Add($"Completed request for {url}");
+            }
+
+            return results;
+        }
+
     }
 }
