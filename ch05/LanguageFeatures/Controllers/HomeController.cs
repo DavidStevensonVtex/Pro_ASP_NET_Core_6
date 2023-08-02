abc3801ebd4247eb7bdfe7219db8fca0100a313a
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 10:07:32 -0400

    Listing 5-44. Creating an Anonymous Type in the HomeController.cs File in the Controllers Folder

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -4,8 +4,14 @@
     {
         public ViewResult Index()
         {
-            var names = new[] { "Kayak", "Lifejacket", "Soccer ball" };
-            return View(names);
+            var products = new[]
+            {
+                new { Name = "Kayak", Price = 275M },
+                new { Name = "Lifejacket", Price = 48.95M },
+                new { Name = "Soccer ball", Price = 19.50M },
+                new { Name = "Corner flag", Price = 34.95M }
+            };
+            return View(products.Select(p => p.Name));
         }
     }
 }
