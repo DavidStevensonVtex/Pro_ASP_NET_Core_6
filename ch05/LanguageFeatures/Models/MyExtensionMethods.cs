235a3dc8512f05957ea9d798a112d20f5ce590e7
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 09:00:29 -0400

    Listing 5-27. ShoppingCart.cs Listing 5-28. MyExtensionMethods.cs Files in the Models Folder

---
+++ ch05/LanguageFeatures/Models/MyExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethods
+    {
+        public static decimal TotalPrices(this ShoppingCart cartParam)
+        {
+            decimal total = 0;
+            if (cartParam.Products != null)
+            {
+                foreach ( Product? prod in cartParam.Products)
+                {
+                    total += prod?.Price ?? 0;
+                }
+            }
+
+            return total;
+        }
+    }
+}
---
+++ ch05/LanguageFeatures/Models/ShoppingCart.cs
@@ -0,0 +1,7 @@
+﻿namespace LanguageFeatures.Models
+{
+    public class ShoppingCart
+    {
+        public IEnumerable<Product?>? Products { get; set; }
+    }
+}
