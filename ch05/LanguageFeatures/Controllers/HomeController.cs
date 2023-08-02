45574645d34281516f110fac68b0795a425965d7
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 09:56:31 -0400

    Listing5-40. Creating a Common Action Pattern in the HomeController.cs File in the Controllers Folder

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -4,22 +4,7 @@
     {
         public ViewResult Index()
         {
-            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
-            Product[] productArray =
-            {
-                new Product { Name = "Kayak", Price = 275M },
-                new Product { Name = "Lifejacket", Price = 48.95M },
-                new Product { Name = "Soccer ball", Price = 19.50M },
-                new Product { Name = "Corner flag", Price = 34.95M }
-            };
-
-            decimal priceFilterTotal = productArray.Filter(p => (p?.Price ?? 0) >= 20).TotalPrices();
-            decimal nameFilterTotal = productArray.Filter(p => p?.Name?[0] == 'S').TotalPrices();
-
-            return View(new string[] { 
-                $"Price Total: {priceFilterTotal:C2}",
-                $"Name Total: {nameFilterTotal:C2}"
-            } );
+            return View(Product.GetProducts().Select(p => p?.Name));
         }
     }
 }
