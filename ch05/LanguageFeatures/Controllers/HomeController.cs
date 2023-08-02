aa0fd3ce4534cc5b972d864639aff52ab8e9bd8f
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 09:59:14 -0400

    Listing 5-41. A Lambda Action Method in the HomeController.cs File in the Controllers Folder

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -2,9 +2,7 @@
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
-        {
-            return View(Product.GetProducts().Select(p => p?.Name));
-        }
+        public ViewResult Index() => 
+            View(Product.GetProducts().Select(p => p?.Name));
     }
 }
