0b1bf06bb46c6e91d10a68b2b3b9aad596bf3e0c
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 08:49:19 -0400

    Listing 5-24. Using a Target-Typed new Expression in the HomeController.cs File in the Controllers Folder

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -4,7 +4,7 @@
     {
         public ViewResult Index()
         {
-            Dictionary<string, Product> products = new Dictionary<string, Product>
+            Dictionary<string, Product> products = new()
             {
                 ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
                 ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
