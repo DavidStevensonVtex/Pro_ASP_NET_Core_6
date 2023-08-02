628440eab66f6191167a990b3e3e2cae6d489527
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 09:04:48 -0400

    Listing 5-29. Applying an Extension Method in the HomeController.cs File in the Controllers Folder

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -4,21 +4,9 @@
     {
         public ViewResult Index()
         {
-            object[] data = new object[] { 275M, 29.95M, "apple", "orange", 100, 10 };
-            decimal total = 0;
-            for ( int i = 0; i < data.Length; i++ )
-            {
-                switch(data[i])
-                {
-                    case decimal decimalValue:
-                        total += decimalValue;
-                        break;
-                    case int intValue when intValue > 50:
-                        total += intValue;
-                        break;
-                }
-            }
-            return View(new string[] { $"Total: {total:C2}" });
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            decimal cartTotal = cart.TotalPrices();
+            return View(new string[] { $"Total: {cartTotal:C2}" });
         }
     }
 }
