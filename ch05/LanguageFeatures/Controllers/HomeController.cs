d70db601ca662825f195893f40a4a07c63100ae8
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 09:30:01 -0400

    Listing 5-34. Using the Filtering Extension Method in the HomeController.cs File in the Controllers Folder

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -8,12 +8,13 @@
             Product[] productArray =
             {
                 new Product { Name = "Kayak", Price = 275M },
-                new Product { Name = "Lifejacket", Price = 48.95M }
+                new Product { Name = "Lifejacket", Price = 48.95M },
+                new Product { Name = "Soccer ball", Price = 19.50M },
+                new Product { Name = "Corner flag", Price = 34.95M }
             };
 
-            decimal cartTotal = cart.TotalPrices();
-            decimal arrayTotal = productArray.TotalPrices();
-            return View(new string[] { $"Cart Total: {cartTotal:C2}", $"Array Total: ${arrayTotal:C2}" } );
+            decimal arrayTotal = productArray.FilterByPrice(20).TotalPrices();
+            return View(new string[] { $"Array Total: {arrayTotal:C2}" } );
         }
     }
 }
