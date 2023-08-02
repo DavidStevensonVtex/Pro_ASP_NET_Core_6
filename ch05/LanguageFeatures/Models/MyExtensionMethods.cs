35225295dcc1682612d07cf7a565f54584c93de0
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 09:12:01 -0400

    Listing 5-31. Updating an Extension Method in the MyExtensionMethods.cs File in the Models Folder

--- ch05/LanguageFeatures/Models/MyExtensionMethods.cs
+++ ch05/LanguageFeatures/Models/MyExtensionMethods.cs
@@ -4,17 +4,13 @@ namespace LanguageFeatures.Models
 {
     public static class MyExtensionMethods
     {
-        public static decimal TotalPrices(this ShoppingCart cartParam)
+        public static decimal TotalPrices(this IEnumerable<Product?> products)
         {
             decimal total = 0;
-            if (cartParam.Products != null)
+            foreach (Product? prod in products)
             {
-                foreach ( Product? prod in cartParam.Products)
-                {
-                    total += prod?.Price ?? 0;
-                }
+                total += prod?.Price ?? 0;
             }
-
             return total;
         }
     }
