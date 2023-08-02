dd4d6a126168316de6457eecb2e16a66dfbe0fa1
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 09:08:03 -0400

    Listing 5-30. Implementing an Interface in the ShoppingCart.cs File in the Models Folder

--- ch05/LanguageFeatures/Models/ShoppingCart.cs
+++ ch05/LanguageFeatures/Models/ShoppingCart.cs
@@ -1,7 +1,14 @@
-﻿namespace LanguageFeatures.Models
+﻿using System.Collections;
+
+namespace LanguageFeatures.Models
 {
-    public class ShoppingCart
+    public class ShoppingCart : IEnumerable<Product?>
     {
         public IEnumerable<Product?>? Products { get; set; }
+
+        public IEnumerator<Product?> GetEnumerator() => Products?.GetEnumerator() ??
+            Enumerable.Empty<Product?>().GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
