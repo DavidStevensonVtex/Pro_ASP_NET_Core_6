7afe423612f5e8e6044f60c94c5138d9beeabfec
David Stevenson <drstevenson1958@yahoo.com>
2023-08-02 10:37:19 -0400

    Listing 5-53. An Asynchronous Action Method in the HomeController.cs File in the Controllers Folder

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -2,16 +2,10 @@
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public async Task<ViewResult> Index()
         {
-            IProductSelection cart = new ShoppingCart(
-                new Product { Name = "Kayak", Price = 275M },
-                new Product { Name = "Lifejacket", Price = 48.95M },
-                new Product { Name = "Soccer ball", Price = 19.50M },
-                new Product { Name = "Corner flag", Price = 34.95M }
-            );
-
-            return View(cart.Names);
+            long? length = await MyAsyncMethods.GetPageLength();
+            return View(new string[] { $"Length: {length}" });
         }
     }
 }
