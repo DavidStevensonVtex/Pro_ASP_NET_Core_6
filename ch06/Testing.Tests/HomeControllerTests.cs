25cd171704fd585fd31329ed3e2a982c1fa3ac3c
David Stevenson <drstevenson1958@yahoo.com>
2023-08-03 10:25:21 -0400

    Listing 6-18. Isolating the Controller in the HomeControllerTests.cs file in the Unit Tests project

--- ch06/Testing.Tests/HomeControllerTests.cs
+++ ch06/Testing.Tests/HomeControllerTests.cs
@@ -12,22 +12,33 @@ namespace Testing.Tests
 {
     public class HomeControllerTests
     {
+        class FakeDataSource : IDataSource
+        {
+            public FakeDataSource(Product[] data) => Products = data;
+            public IEnumerable<Product> Products { get; set; }
+        }
+
         [Fact]
         public void IndexActionModelIsComplete()
         {
             // Arrange
-            var controller = new HomeController();
-            Product[] products = new Product[]
+            Product[] testData = new Product[]
             {
-                new Product { Name = "Kayak", Price = 275M },
-                new Product { Name = "Lifejacket", Price = 48.95M }
+                new Product { Name = "P1", Price = 75.10M },
+                new Product { Name = "P2", Price = 120M },
+                new Product { Name = "P3", Price = 110M }
             };
+
+            IDataSource data = new FakeDataSource(testData);
+            var controller = new HomeController();
+            controller.dataSource = data;
 
             // Act
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
 
             // Assert
-            Assert.Equal(products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+            Assert.NotNull(model);
+            Assert.Equal(data.Products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
         }
     }
 }
