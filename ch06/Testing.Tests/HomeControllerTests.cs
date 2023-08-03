a272cf289081f7661195dfaf6de53895d17bfb55
David Stevenson <drstevenson1958@yahoo.com>
2023-08-03 10:33:28 -0400

    Listing 6-20. Creating a Mock Object in the HomeControllerTests.cs File in the Unit Test project

--- ch06/Testing.Tests/HomeControllerTests.cs
+++ ch06/Testing.Tests/HomeControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,6 @@ namespace Testing.Tests
 {
     public class HomeControllerTests
     {
-        class FakeDataSource : IDataSource
-        {
-            public FakeDataSource(Product[] data) => Products = data;
-            public IEnumerable<Product> Products { get; set; }
-        }
-
         [Fact]
         public void IndexActionModelIsComplete()
         {
@@ -29,16 +24,18 @@ namespace Testing.Tests
                 new Product { Name = "P3", Price = 110M }
             };
 
-            IDataSource data = new FakeDataSource(testData);
+            var mock = new Mock<IDataSource>();
+            mock.Setup(m => m.Products).Returns(testData);
             var controller = new HomeController();
-            controller.dataSource = data;
+            controller.dataSource = mock.Object;
 
             // Act
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
 
             // Assert
             Assert.NotNull(model);
-            Assert.Equal(data.Products, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+            Assert.Equal(testData, model, Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && p1?.Price == p2?.Price));
+            mock.VerifyGet(m => m.Products, Times.Once);
         }
     }
 }
