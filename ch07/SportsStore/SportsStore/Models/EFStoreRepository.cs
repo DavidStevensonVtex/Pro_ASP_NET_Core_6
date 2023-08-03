b55c59ae6aab2a7cd80fc21d0aaf5e8018c47dcd
David Stevenson <drstevenson1958@yahoo.com>
2023-08-03 11:27:38 -0400

    Listing 7-19. The Contents of the EFStoreRepository.cs file in the SportsStore/Models folder

---
+++ ch07/SportsStore/SportsStore/Models/EFStoreRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace SportsStore.Models
+{
+    public class EFStoreRepository : IStoreRepository
+    {
+        private StoreDbContext context;
+
+        public EFStoreRepository(StoreDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Product> Products => context.Products;
+    }
+}
