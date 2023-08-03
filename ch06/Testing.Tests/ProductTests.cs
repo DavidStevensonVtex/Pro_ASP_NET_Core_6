9165d99964bccba44eb3a49f01ba8af9535b0995
David Stevenson <drstevenson1958@yahoo.com>
2023-08-03 09:44:06 -0400

    Listing 6-10. The Contents of the ProductTests.cs File in the Unit Test project

---
+++ ch06/Testing.Tests/ProductTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testing.Models;
+using Xunit;
+
+namespace Testing.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            // Arrange
+            var p = new Product { Name = "Test", Price = 100M };
+
+            // Act
+            p.Name = "New Name";
+
+            // Assert 
+            Assert.Equal("New Name", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            // Arrange
+            var p = new Product { Name = "Test", Price = 100M };
+
+            // Act
+            p.Price = 200M;
+
+            // Assert
+            Assert.Equal(200M, p.Price);
+        }
+    }
+}
