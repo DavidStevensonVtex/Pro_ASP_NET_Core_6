2aa3d5bff449fc34f113dadb75a55c88611b4fe3
David Stevenson <drstevenson1958@yahoo.com>
2023-08-03 09:17:19 -0400

    Listing 6-3. Enabling the MVC Framework in the Program.cs File in the SimpleApp Folder

--- ch06/Testing/Program.cs
+++ ch06/Testing/Program.cs
@@ -6,8 +6,7 @@ namespace Testing
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddRazorPages();
+			builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 			var app = builder.Build();
 
@@ -22,7 +21,9 @@ namespace Testing
 
 			app.UseAuthorization();
 
-			app.MapRazorPages();
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
 
 			app.Run();
 		}
