7abb6183e0a645f84e16f55d9839f9b39245d836
David Stevenson <drstevenson1958@yahoo.com>
2023-08-03 09:21:01 -0400

    Listing 6-4. The Contents of the Product.cs File  in the Models folder.

---
+++ ch06/Testing/Models/Productd.cs
@@ -0,0 +1,15 @@
+﻿namespace Testing.Models
+{
+	public class Product
+	{
+		public string Name { get; set; }
+		public decimal? Price { get; set; }
+
+		public static Product[] GetProducts()
+		{
+			Product kayak = new Product { Name = "Kayak", Price = 275M };
+			Product lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
+			return new Product[] { kayak, lifejacket };
+		}
+	}
+}
