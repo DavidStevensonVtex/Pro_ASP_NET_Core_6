aea9d5e611cb6b40150356b5751b7faf25bd4591
David Stevenson <drstevenson1958@yahoo.com>
2023-08-03 10:10:30 -0400

    Listing 6-16. Creating a Data Source in the Product.cs File in the Models folder

--- ch06/Testing/Models/Product.cs
+++ ch06/Testing/Models/Product.cs
@@ -1,15 +1,26 @@
-﻿namespace Testing.Models
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Testing.Models
 {
 	public class Product
 	{
 		public string Name { get; set; } = string.Empty;
 		public decimal? Price { get; set; }
 
-		public static Product[] GetProducts()
-		{
-			Product kayak = new Product { Name = "Kayak", Price = 275M };
-			Product lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
-			return new Product[] { kayak, lifejacket };
-		}
+		//public static Product[] GetProducts()
+		//{
+		//	Product kayak = new Product { Name = "Kayak", Price = 275M };
+		//	Product lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
+		//	return new Product[] { kayak, lifejacket };
+		//}
 	}
+
+    public class ProductDataSource : IDataSource
+    {
+		public IEnumerable<Product> Products => new Product[]
+		{
+			new Product { Name = "Kayak", Price = 275M },
+			new Product { Name = "Lifejacket", Price = 48.95M }
+		};
+    }
 }
