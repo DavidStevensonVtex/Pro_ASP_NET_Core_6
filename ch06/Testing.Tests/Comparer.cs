4ab4ced3bc50354dc9951043a66a5aea054c7ffd
David Stevenson <drstevenson1958@yahoo.com>
2023-08-03 09:56:59 -0400

    Listing 6-13. The Contents of the Comparer.cs File in the Unit Tests project

---
+++ ch06/Testing.Tests/Comparer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing.Tests
+{
+    public class Comparer
+    {
+        public static Comparer<U?> Get<U>(Func<U?, U?, bool> func) {
+            return new Comparer<U?>(Func) ;
+    }
+
+    public class Comparer<T> : Comparer, IEqualityComparer<T>
+    {
+        private Func<T?, T?, bool> comparisonFunction;
+
+        public Comparer(Func<T?, T?, bool> func)
+        {
+            comparisonFunction = func;
+        }
+
+        public bool Equals(T? x, T? y)
+        {
+            return comparisonFunction(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj?.GetHashCode() ?? 0;
+        }
+    }
+}
