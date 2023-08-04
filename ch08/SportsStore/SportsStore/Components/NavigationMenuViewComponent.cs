d9454be767053e51532599de80820ab03bbe69c9
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 13:22:55 -0400

    Listing 8-8. Adding Categories in the NavigationalMenuViewComponent.cs file in the Components folder

--- ch08/SportsStore/SportsStore/Components/NavigationMenuViewComponent.cs
+++ ch08/SportsStore/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Components
 {
 	public class NavigationMenuViewComponent : ViewComponent
 	{
-		public string Invoke()
+		public IStoreRepository repository;
+
+		public NavigationMenuViewComponent(IStoreRepository repo)
 		{
-			return "Hello from the Nav View Component!";
+			this.repository = repo;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			return View(repository.Products
+				.Select(x => x.Category)
+				.Distinct()
+				.OrderBy(x => x));
 		}
 	}
 }
