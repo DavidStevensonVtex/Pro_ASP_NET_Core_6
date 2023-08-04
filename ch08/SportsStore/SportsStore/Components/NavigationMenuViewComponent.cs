8292865105427fd6934c7c94ae356d7b05640725
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 13:16:36 -0400

    Listing 8-6. The Contents of the NavigationMenuViewComponent.cs file in the Components folder

---
+++ ch08/SportsStore/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Components
+{
+	public class NavigationMenuViewComponent : ViewComponent
+	{
+		public string Invoke()
+		{
+			return "Hello from the Nav View Component!";
+		}
+	}
+}
