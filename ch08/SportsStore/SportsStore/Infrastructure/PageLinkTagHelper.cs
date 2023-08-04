ca33ec6553f54026d46e19885c4b374862eea146
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 13:09:18 -0400

    Listing 8-4. Prefixed Values in the PageLinkTagHelper.cs File in the Infrastructure folder

--- ch08/SportsStore/SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ ch08/SportsStore/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -22,6 +22,9 @@ namespace SportsStore.Infrastructure
 		public PagingInfo? PageModel { get; set; }
 		public string? PageAction { get; set; }
 
+		[HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+		public Dictionary<string, object> PageUrlValues { get; set; } = new Dictionary<string, object>();
+
 		public bool PageClassesEnabled { get; set; } = false;
 		public string PageClass { get; set; } = String.Empty;
 		public string PageClassNormal { get; set; } = String.Empty;
@@ -36,7 +39,8 @@ namespace SportsStore.Infrastructure
 				for (int i = 1; i <= PageModel.TotalPages; i++)
 				{
 					TagBuilder tag = new TagBuilder("a");
-					tag.Attributes["href"] = urlHelper.Action(PageAction, new { ProductPage = i });
+					PageUrlValues["productPage"] = i;
+					tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
 					if (PageClassesEnabled)
 					{
 						tag.AddCssClass(PageClass);
