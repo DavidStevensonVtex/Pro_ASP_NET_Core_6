4177bb43d92cfd0e68dbb415d00937c6870ce74c
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 10:09:53 -0400

    Unit Test: Pagination

--- ch07/SportsStore/SportsStore.Tests/HomeControllerTests.cs
+++ ch07/SportsStore/SportsStore.Tests/HomeControllerTests.cs
@@ -36,5 +36,34 @@ namespace SportsStore.Tests
 			Assert.Equal("P2", prodArray[1].Name);
 
 		}
+
+		[Fact]
+		public void Can_Paginate()
+		{
+			// Arrange
+			Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+			mock.Setup(m => m.Products).Returns(new Product[]
+			{
+				new Product { ProductID = 1, Name = "P1" },
+				new Product { ProductID = 2, Name = "P2" },
+				new Product { ProductID = 3, Name = "P3" },
+				new Product { ProductID = 4, Name = "P4" },
+				new Product { ProductID = 5, Name = "P5" },
+			}.AsQueryable<Product>());
+
+			HomeController controller = new HomeController(mock.Object);
+			controller.PageSize = 3;
+
+			// Act
+			IEnumerable<Product> result = 
+				(controller.Index(2) as ViewResult)?.ViewData.Model as IEnumerable<Product> 
+				?? Enumerable.Empty<Product>();
+
+			// Assert
+			Product[] prodArray = result.ToArray();
+			Assert.True(prodArray.Length == 2);
+			Assert.Equal("P4", prodArray[0].Name);
+			Assert.Equal("P5", prodArray[1].Name);
+		}
 	}
 }
