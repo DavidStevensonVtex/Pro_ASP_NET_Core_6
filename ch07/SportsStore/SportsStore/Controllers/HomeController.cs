de9aecfe3d40b067e9988c7d56dbf528ce790d3f
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 11:15:36 -0400

    Listing 7-31. Updating the Action method in the HomeController.cs file in the Controllers folder

--- ch07/SportsStore/SportsStore/Controllers/HomeController.cs
+++ ch07/SportsStore/SportsStore/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
@@ -14,9 +15,19 @@ namespace SportsStore.Controllers
         }
 
         public ViewResult Index(int productPage = 1) =>
-            View(repository.Products
-                .OrderBy(p => p.ProductID)
-                .Skip((productPage - 1) * PageSize)
-                .Take(PageSize));
-    }
+            View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
+
+	}
 }
