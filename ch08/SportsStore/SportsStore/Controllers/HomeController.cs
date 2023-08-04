f90a7c3d7cb1b9b78862a983eadd13db9aaca3eb
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 12:41:39 -0400

    Listing 8-2. Adding Category Support in the HomeController.cs file in the Controllers folder

--- ch08/SportsStore/SportsStore/Controllers/HomeController.cs
+++ ch08/SportsStore/SportsStore/Controllers/HomeController.cs
@@ -14,10 +14,11 @@ namespace SportsStore.Controllers
             repository = repo;
         }
 
-        public ViewResult Index(int productPage = 1) =>
+        public ViewResult Index(string? category, int productPage = 1) =>
             View(new ProductsListViewModel
             {
                 Products = repository.Products
+                    .Where(p => category == null || p.Category == category)
                     .OrderBy(p => p.ProductID)
                     .Skip((productPage - 1) * PageSize)
                     .Take(PageSize),
@@ -26,7 +27,8 @@ namespace SportsStore.Controllers
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-                }
+                },
+                CurrentCategory = category
             });
 
 	}
