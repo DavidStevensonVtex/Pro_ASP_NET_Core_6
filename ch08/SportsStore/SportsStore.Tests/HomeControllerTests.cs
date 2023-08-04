d0210ed3a6e801eba89e595689d75ea10bd83ac3
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 12:51:18 -0400

    Unit Test: Category Filtering

--- ch08/SportsStore/SportsStore.Tests/HomeControllerTests.cs
+++ ch08/SportsStore/SportsStore.Tests/HomeControllerTests.cs
@@ -91,5 +91,31 @@ namespace SportsStore.Tests
 			Assert.Equal(5, pageInfo.TotalItems);
 			Assert.Equal(2, pageInfo.TotalPages);
 		}
+
+		[Fact]
+		public void Can_Filter_Products()
+		{
+			// Arrange
+			// - create the mock repository
+			Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+			mock.Setup(m => m.Products).Returns(new Product[]
+			{
+				new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+				new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
+				new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
+				new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
+				new Product { ProductID = 5, Name = "P5", Category = "Cat3" },
+			}.AsQueryable<Product>());
+
+			HomeController controller = new HomeController(mock.Object) { PageSize = 3 };
+
+			// Act
+			Product[] result = (controller.Index("Cat2", 1)?.ViewData.Model as ProductsListViewModel ?? new()).Products.ToArray();
+
+			// Assert
+			Assert.Equal(2, result.Length);
+			Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
+			Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
+		}
 	}
 }
