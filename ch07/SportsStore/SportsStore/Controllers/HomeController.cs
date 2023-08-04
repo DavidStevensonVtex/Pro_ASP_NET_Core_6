f7daa22a923fe54acf12d1a0a49263bbbb0b5d20
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 10:03:41 -0400

    Listing 7-26. Adding Pagination in the HomeController.cs File in the Controllers folder

--- ch07/SportsStore/SportsStore/Controllers/HomeController.cs
+++ ch07/SportsStore/SportsStore/Controllers/HomeController.cs
@@ -6,12 +6,17 @@ namespace SportsStore.Controllers
     public class HomeController : Controller
     {
         public IStoreRepository repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repo)
         {
             repository = repo;
         }
 
-        public IActionResult Index() => View(repository.Products);
+        public ViewResult Index(int productPage = 1) =>
+            View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
     }
 }
