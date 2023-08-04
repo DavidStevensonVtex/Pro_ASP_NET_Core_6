153153a5b4064b1af30166d94b0dfdda074312cb
David Stevenson <drstevenson1958@yahoo.com>
2023-08-04 12:28:20 -0400

    Chapter 8: SportsStore: Navigation and Cart

---
+++ ch08/SportsStore/SportsStore.Tests/PageLinkHelperTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Models;
+using SportsStore.Controllers;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SportsStore.Infrastructure;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SportsStore.Tests
+{
+	public class PageLinkHelperTests
+	{
+		[Fact]
+		public void Can_Generate_Page_Links()
+		{
+			// Arrange
+			var urlHelper = new Mock<IUrlHelper>();
+			urlHelper.SetupSequence(x => x.Action(It.IsAny<UrlActionContext>()))
+				.Returns("Test/Page1")
+				.Returns("Test/Page2")
+				.Returns("Test/Page3");
+
+			var urlHelperFactory = new Mock<IUrlHelperFactory>();
+			urlHelperFactory.Setup(f =>
+				f.GetUrlHelper(It.IsAny<ActionContext>()))
+					.Returns(urlHelper.Object);
+
+			var viewContext = new Mock<ViewContext>();
+
+			PageLinkTagHelper helper =
+				new PageLinkTagHelper(urlHelperFactory.Object)
+				{
+					PageModel = new Models.ViewModels.PagingInfo
+					{
+						CurrentPage = 2,
+						TotalItems = 28,
+						ItemsPerPage = 10
+					},
+					ViewContext = viewContext.Object,
+					PageAction = "Test"
+				};
+
+			TagHelperContext ctx = new TagHelperContext(
+				new TagHelperAttributeList(),
+				new Dictionary<object, object>(), "");
+
+			var content = new Mock<TagHelperContent>();
+			//TagHelperOutput output = new TagHelperOutput("div",
+			//	new TagHelperAttributeList(),
+			//	getChildContentAsync: (useCachedResult, encoder) => 
+			//		Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+
+			TagHelperOutput output = new TagHelperOutput("div",
+				new TagHelperAttributeList(),
+				(cache, encoder) => Task.FromResult(content.Object));
+
+
+			// Act 
+			helper.Process(ctx, output);
+
+			// Assert
+			var contentOutput = output.Content.GetContent();
+			Assert.Equal(
+				@"<a href=""Test/Page1"">1</a>" +
+				@"<a href=""Test/Page2"">2</a>" +
+				@"<a href=""Test/Page3"">3</a>",
+				contentOutput);
+		}
+	}
+}
---
+++ ch08/SportsStore/SportsStore/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            builder.Services.AddDbContext<StoreDbContext>(opts =>
+            {
+                opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+            });
+
+            builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute("pagination",
+                "Products/Page{productPage}",
+                new { Controller = "Home", action = "Index" });
+
+            app.MapDefaultControllerRoute();
+
+            SeedData.EnsurePopulated(app);
+
+            app.Run();
+        }
+    }
+}
