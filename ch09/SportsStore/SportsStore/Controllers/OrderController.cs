77f9143b728f0b056341c7a8743bb690a140005b
David Stevenson <drstevenson1958@yahoo.com>
2023-08-05 13:26:20 -0400

    Listing 9-13. The Contents of the OrderController.cs file in the Controllers folder

---
+++ ch09/SportsStore/SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Index() => View();
+    }
+}
