36ffa567c32605013995cfcbaa3edee29e634991
David Stevenson <drstevenson1958@yahoo.com>
2023-08-05 12:51:33 -0400

    Listing 9-5. Removing Cart items in the Cart.cshtml file in the Pages folder

--- ch09/SportsStore/SportsStore/Program.cs
+++ ch09/SportsStore/SportsStore/Program.cs
@@ -21,6 +21,8 @@ namespace SportsStore
             builder.Services.AddRazorPages();
             builder.Services.AddDistributedMemoryCache();
             builder.Services.AddSession();
+            builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             var app = builder.Build();
 
@@ -31,8 +33,6 @@ namespace SportsStore
             }
             app.UseStaticFiles();
             app.UseSession();
-            builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
-            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
             app.UseRouting();
 
