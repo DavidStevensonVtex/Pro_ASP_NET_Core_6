51dc3280b73b0e5a5426be8fc1c50e0c32f732fa
David Stevenson <drstevenson1958@yahoo.com>
2023-08-06 15:36:48 -0400

    Chapter 10: SportsStore: Administration

---
+++ ch10/SportsStore/SportsStore.Tests/OrderControllerTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Routing;
+using Moq;
+using SportsStore.Components;
+using SportsStore.Controllers;
+using SportsStore.Models;
+
+namespace SportsStore.Tests
+{
+	public class OrderControllerTests
+	{
+		[Fact]
+		public void Cannot_Checkout_Empty_Cart()
+		{
+			// Arrange - create a mock repository
+			Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+			// Arrange - create an empty cart
+			Cart cart = new Cart();
+			// Arrange - create the order
+			Order order = new Order();
+			// Arrange - create an instance of the controller
+			OrderController target = new OrderController(mock.Object, cart);
+
+			// Act
+			ViewResult? result = target.Checkout(order) as ViewResult;
+
+			// Assert - check that the order hasn't been stored.
+			mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+			// Assert - check that the method is returning the default view
+			Assert.True(string.IsNullOrEmpty(result?.ViewName));
+			// Assert - check that I am passing an invalid model to the view
+			Assert.False(result?.ViewData.ModelState.IsValid);
+		}
+
+		[Fact]
+		public void Cannot_Checkout_Invalid_ShippingDetails()
+		{
+			// Arrange - create a mock order repository
+			Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+			// Arrange - create a cart with one item
+			Cart cart = new Cart();
+			cart.AddItem(new Product(), 1);
+			// Arrange - create an instance of the controller
+			OrderController target = new OrderController(mock.Object, cart);
+			// Arrange - add an error to the model
+			target.ModelState.AddModelError("error", "error");
+
+			// Act
+			ViewResult? result = target.Checkout(new Order()) as ViewResult;
+
+			// Assert - check that the order hasn't been stored.
+			mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+			// Assert - check that the method is returning the default view
+			Assert.True(string.IsNullOrEmpty(result?.ViewName));
+			// Assert - check that I am passing an invalid model to the view
+			Assert.False(result?.ViewData.ModelState.IsValid);
+		}
+
+		[Fact]
+		public void Can_Checkout_And_Submit_Order()
+		{
+			// Arrange - create a mock order repository
+			Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+			// Arrange - create a cart with one item
+			Cart cart = new Cart();
+			cart.AddItem(new Product(), 1);
+			// Arrange - create the order
+			Order order = new Order();
+			// Arrange - create an instance of the controller
+			OrderController target = new OrderController(mock.Object, cart);
+
+			// Act - try to Checkout
+			RedirectToPageResult? result = target.Checkout(new Order()) as RedirectToPageResult;
+
+			// Assert - check that the order has been stored
+			mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Once);
+			// Assert - check that the method is redirecting to the Completed section
+			Assert.Equal("/Completed", result?.PageName);
+		}
+	}
+}
---
+++ ch10/SportsStore/SportsStore.Tests/PageLinkHelperTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using SportsStore.Infrastructure;
+
+namespace SportsStore.Tests
+{
+    public class PageLinkHelperTests
+	{
+		[Fact]
+		public void Can_Generate_Page_Links()
+		{
+			// Arrange
+			var urlHelper = new Mock<IUrlHelper>();
+			urlHelper.SetupSequence(x => x.Action(It.IsAny<UrlActionContext>()))
+				.Returns("Test/Page1")
+				.Returns("Test/Page2")
+				.Returns("Test/Page3");
+
+			var urlHelperFactory = new Mock<IUrlHelperFactory>();
+			urlHelperFactory.Setup(f =>
+				f.GetUrlHelper(It.IsAny<ActionContext>()))
+					.Returns(urlHelper.Object);
+
+			var viewContext = new Mock<ViewContext>();
+
+			PageLinkTagHelper helper =
+				new PageLinkTagHelper(urlHelperFactory.Object)
+				{
+					PageModel = new Models.ViewModels.PagingInfo
+					{
+						CurrentPage = 2,
+						TotalItems = 28,
+						ItemsPerPage = 10
+					},
+					ViewContext = viewContext.Object,
+					PageAction = "Test"
+				};
+
+			TagHelperContext ctx = new TagHelperContext(
+				new TagHelperAttributeList(),
+				new Dictionary<object, object>(), "");
+
+			var content = new Mock<TagHelperContent>();
+			//TagHelperOutput output = new TagHelperOutput("div",
+			//	new TagHelperAttributeList(),
+			//	getChildContentAsync: (useCachedResult, encoder) => 
+			//		Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+
+			TagHelperOutput output = new TagHelperOutput("div",
+				new TagHelperAttributeList(),
+				(cache, encoder) => Task.FromResult(content.Object));
+
+
+			// Act 
+			helper.Process(ctx, output);
+
+			// Assert
+			var contentOutput = output.Content.GetContent();
+			Assert.Equal(
+				@"<a href=""Test/Page1"">1</a>" +
+				@"<a href=""Test/Page2"">2</a>" +
+				@"<a href=""Test/Page3"">3</a>",
+				contentOutput);
+		}
+	}
+}
