e535bd4dee226c32ca286d571072927122802c6b
David Stevenson <drstevenson1958@yahoo.com>
2023-08-06 13:17:41 -0400

    Listing 9-15. Adding a Property in the SToreDbContext.cs file in the SportsStore/Models folder
    Extending the Database

--- ch09/SportsStore/SportsStore/Models/StoreDbContext.cs
+++ ch09/SportsStore/SportsStore/Models/StoreDbContext.cs
@@ -7,5 +7,6 @@ namespace SportsStore.Models
         public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options) { }
 
         public DbSet<Product> Products => Set<Product>();
+        public DbSet<Order> Orders => Set<Order>();
     }
 }
