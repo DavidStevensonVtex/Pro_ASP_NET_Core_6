2a12ac48069769bfc62bc6a3ded841786b97839d
David Stevenson <drstevenson1958@yahoo.com>
2023-08-06 17:11:51 -0400

    Listing 10-13. Implementing Methods in the EFStoreRepository.cs file in the Models folder

--- ch10/SportsStore/SportsStore/Models/EFStoreRepository.cs
+++ ch10/SportsStore/SportsStore/Models/EFStoreRepository.cs
@@ -10,5 +10,22 @@
         }
 
         public IQueryable<Product> Products => context.Products;
+
+        public void CreateProduct(Product p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+        }
+
+        public void DeleteProduct(Product p)
+        {
+            context.Remove(p);
+            context.SaveChanges();
+        }
+
+        public void SaveProduct(Product p)
+        {
+            context.SaveChanges(p);
+        }
     }
 }
