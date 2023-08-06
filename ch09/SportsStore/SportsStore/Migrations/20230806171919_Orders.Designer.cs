2c41d253037e3ea74f2d54f5dac1ff6c66485cdb
David Stevenson <drstevenson1958@yahoo.com>
2023-08-06 13:22:54 -0400

    Listing 9-16. Creating a Migration: dotnet ef migrations add Orders

--- ch09/SportsStore/SportsStore/Migrations/20230806171919_Orders.Designer.cs
+++ ch09/SportsStore/SportsStore/Migrations/20230806171919_Orders.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportsStore.Models;
 
@@ -11,9 +12,10 @@ using SportsStore.Models;
 namespace SportsStore.Migrations
 {
     [DbContext(typeof(StoreDbContext))]
-    partial class StoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230806171919_Orders")]
+    partial class Orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
