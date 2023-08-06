d651237970bd6ba43c4613f79fab8f6110f380e4
David Stevenson <drstevenson1958@yahoo.com>
2023-08-06 17:06:58 -0400

    Listing 10-12. Adding Methods in the IStoreRepository.cs file in the Models folder

--- ch10/SportsStore/SportsStore/Models/IStoreRepository.cs
+++ ch10/SportsStore/SportsStore/Models/IStoreRepository.cs
@@ -3,5 +3,9 @@
     public interface IStoreRepository
     {
         IQueryable<Product> Products { get; }
+
+        void SaveProduct(Product p);
+        void CreateProduct(Product p);
+        void DeleteProduct(Product p);
     }
 }
