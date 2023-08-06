5b2b02006f41f39fd2bf49acb82c1822aa275c9b
David Stevenson <drstevenson1958@yahoo.com>
2023-08-06 13:32:23 -0400

    Listing 9-18. The contents of the EFOrderRepository.cs file in the Models folder

---
+++ ch09/SportsStore/SportsStore/Models/EFOrderRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace SportsStore.Models
+{
+	public class EFOrderRepository : IOrderRepository
+	{
+		private StoreDbContext context;
+		public EFOrderRepository(StoreDbContext ctx) 
+		{
+			context = ctx;
+		}
+
+
+		public IQueryable<Order> Orders => context.Orders;
+
+		public void SaveOrder(Order order)
+		{
+			context.AttachRange(order.Lines.Select(l => l.Product));
+			if (order.OrderID == 0)
+			{
+				context.Orders.Add(order);
+			}
+
+			context.SaveChanges();
+		}
+	}
+}
