87160266380619af57816bd4250f6143f1fd73b3
David Stevenson <drstevenson1958@yahoo.com>
2023-08-07 09:49:14 -0400

    Chapter 11: SportsStore: Security and Deployment

---
+++ ch11/SportsStore/SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        private IOrderRepository repository;
+        private Cart cart;
+
+        public OrderController(IOrderRepository repo, Cart cartService)
+        {
+            repository = repo;
+            cart = cartService;
+        }
+
+        [HttpGet]
+		public ViewResult Checkout() => View();
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                repository.SaveOrder(order);
+                cart.Clear();
+                return RedirectToPage("/Completed", new { orderId = order.OrderID });
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
