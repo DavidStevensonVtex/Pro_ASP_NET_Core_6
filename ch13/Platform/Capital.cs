7d5a7bf5f726fb073f7781752a5caaac527edba3
David Stevenson <drstevenson1958@yahoo.com>
2023-08-08 12:45:45 -0400

    Listing 13-9. Depending on the Route Data in the Capital.cs file in the Platform folder

--- ch13/Platform/Capital.cs
+++ ch13/Platform/Capital.cs
@@ -2,46 +2,29 @@
 {
 	public class Capital
 	{
-		private RequestDelegate? next;
-
-		public Capital() { }
-
-		public Capital(RequestDelegate nextDelegate)
+		public static async Task Endpoint(HttpContext context)
 		{
-			next = nextDelegate;
-		}
-
-		public async Task Invoke(HttpContext context)
-		{
-			string[] parts = context.Request.Path.ToString()
-				.Split("/", StringSplitOptions.RemoveEmptyEntries);
-
-			if (parts.Length == 2 && parts[0] == "capital")
+			string? capital = null;
+			string? country = context.Request.RouteValues["country"] as string;
+			switch ((country ?? "").ToLower())
 			{
-				string? capital = null;
-				string country = parts[1];
-				switch (country.ToLower())
-				{
-					case "uk":
-						capital = "London";
-						break;
-					case "france":
-						capital = "Paris";
-						break;
-					case "monaco":
-						context.Response.Redirect($"/population/{country}");
-						break;
-				}
-				if (capital != null)
-				{
-					await context.Response.WriteAsync($"{capital} is the capital of {country}");
-					return;
-				}
+				case "uk":
+					capital = "London";
+					break;
+				case "france":
+					capital = "Paris";
+					break;
+				case "monaco":
+					context.Response.Redirect($"/population/{country}");
+					break;
 			}
-
-			if (next != null)
+			if (capital != null)
 			{
-				await next(context);
+				await context.Response.WriteAsync($"{capital} is the capital of {country}");
+			}
+			else
+			{
+				context.Response.StatusCode = StatusCodes.Status404NotFound;
 			}
 		}
 	}
--- ch13/Platform/Program.cs
+++ ch13/Platform/Program.cs
@@ -9,22 +9,19 @@ namespace Platform
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.Configure<MessageOptions>(options => options.CityName = "Albany");
-
             var app = builder.Build();
 
-            app.MapGet("routing", async context =>
+            app.MapGet("{first}/{second}/{third}", async context =>
             {
-                await context.Response.WriteAsync("Request Was Routed");
+                await context.Response.WriteAsync("Request was routed\n");
+                foreach (var kvp in context.Request.RouteValues)
+                {
+                    await context.Response.WriteAsync($"{kvp.Key}: {kvp.Value}\n");
+                }
             });
 
 			app.MapGet("capital/uk", new Capital().Invoke);
 			app.MapGet("population/paris", new Population().Invoke);
-
-			//app.Run(async (context) =>
-			//{
-			//	await context.Response.WriteAsync("Terminal Middleware Reached");
-			//});
 
 			app.Run();
         }
