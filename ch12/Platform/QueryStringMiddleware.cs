029dc13120ec949403bc1f84cb451c063bca2d6d
David Stevenson <drstevenson1958@yahoo.com>
2023-08-08 10:19:52 -0400

    Listing 12-7. The Contents of the QueryStringMiddleware.cs File in the Platform folder

---
+++ ch12/Platform/QueryStringMiddleware.cs
@@ -0,0 +1,23 @@
+﻿namespace Platform
+{
+	public class QueryStringMiddleware
+	{
+		private RequestDelegate next;
+
+		public QueryStringMiddleware(RequestDelegate nextDelegate)
+		{
+			this.next = nextDelegate;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			if (context.Request.Method == HttpMethods.Get &&
+				context.Request.Query["custom"] == "true")
+			{
+				context.Response.ContentType = "text/plain";
+				await context.Response.WriteAsync("Custom Middleware \n");
+			}
+			await next(context);
+		}
+	}
+}
