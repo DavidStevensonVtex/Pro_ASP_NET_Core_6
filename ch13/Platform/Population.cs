8af2fe605d5ceeaa6754323b6e39dea5f0715100
David Stevenson <drstevenson1958@yahoo.com>
2023-08-08 12:50:14 -0400

    Listing 13-10. Depending on the Route Data in the Population.cs file in the Platform folder

--- ch13/Platform/Population.cs
+++ ch13/Platform/Population.cs
@@ -4,48 +4,32 @@ namespace Platform
 {
 	public class Population
 	{
-		private RequestDelegate? next;
-
-		public Population() { }
-
-		public Population(RequestDelegate nextDelegate)
+		public static async Task Endpoint(HttpContext context)
 		{
-			next = nextDelegate;
-		}
-
-		public async Task Invoke(HttpContext context)
-		{
-			string[] parts = context.Request.Path.ToString()
-				.Split("/", StringSplitOptions.RemoveEmptyEntries);
-
-			if (parts.Length == 2 && parts[0] == "population")
+			string? city = context.Request.RouteValues["city"] as string;
+			int? pop = null;
+			switch ((city ?? "").ToLower())
 			{
-				string city = parts[1];
-				int? pop = null;
-				switch (city.ToLower())
-				{
-					case "london":
-						pop = 8_136_000;
-						break;
-					case "paris":
-						pop = 2_141_000;
-						break;
-					case "monaco":
-						pop = 39_000;
-						break;
-				}
-
-				if (pop.HasValue)
-				{
-					await context.Response.WriteAsync($"City: {city}, Population: {pop}");
-					return;
-				}
+				case "london":
+					pop = 8_136_000;
+					break;
+				case "paris":
+					pop = 2_141_000;
+					break;
+				case "monaco":
+					pop = 39_000;
+					break;
 			}
 
-			if (next != null)
+			if (pop.HasValue)
 			{
-				await next(context);
+				await context.Response.WriteAsync($"City: {city}, Population: {pop}");
+			}
+			else
+			{
+				context.Response.StatusCode = StatusCodes.Status404NotFound;
 			}
 		}
+
 	}
 }
