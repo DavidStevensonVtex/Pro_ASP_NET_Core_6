dae530dc310acb9193cf0db715cabca3a080bb9e
David Stevenson <drstevenson1958@yahoo.com>
2023-08-08 10:31:18 -0400

    Listing 12-9. Adding New Middleware in the Program.cs File in the Platform folder
    Middleware components can modify the HTTPResponse object after the next function has been called, as demonstrated in this code change.

--- ch12/Platform/Program.cs
+++ ch12/Platform/Program.cs
@@ -13,6 +13,12 @@ namespace Platform
 
             app.Use(async (context, next) =>
             {
+                await next();
+                await context.Response.WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
+            });
+
+            app.Use(async (context, next) =>
+            {
                 if (context.Request.Method == HttpMethods.Get &&
                     context.Request.Query["custom"] == "true")
                 {
