3ca31f4ed8e9f323862de5d14683c06ec4b73dfc
David Stevenson <drstevenson1958@yahoo.com>
2023-08-09 13:48:34 -0400

    Listing 14-12. Changing the IMplementation in the TypeBroker.cs file in the Platform/Services folder

--- ch13/Platform/Services/TypeBroker.cs
+++ ch13/Platform/Services/TypeBroker.cs
@@ -2,7 +2,7 @@
 {
 	public class TypeBroker
 	{
-		private static IResponseFormatter formatter = new TextResponseFormatter();
+		private static IResponseFormatter formatter = new HtmlResponseFormatter();
 
 		public static IResponseFormatter Formatter => formatter;
 	}
