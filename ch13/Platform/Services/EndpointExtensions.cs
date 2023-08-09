6ab68ece88d817862d78b21599efaf4f9a5731c5
David Stevenson <drstevenson1958@yahoo.com>
2023-08-09 14:25:12 -0400

    Listing 14-17. The Contents of the EndpointExtensions.cs file in the Services folder

---
+++ ch13/Platform/Services/EndpointExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Platform.Services
+{
+	public static class EndpointExtensions
+	{
+		public static void MapWeather(this IEndpointRouteBuilder app, string path)
+		{
+			IResponseFormatter formatter = app.ServiceProvider.GetRequiredService<IResponseFormatter>();
+			app.MapGet(path, context => Platform.WeatherEndpoint.Endpoint(context, formatter));
+		}
+	}
+}
