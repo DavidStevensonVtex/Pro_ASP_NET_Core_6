b15636349a6e5dbcb3d23b197e4f17b800d9dbfb
David Stevenson <drstevenson1958@yahoo.com>
2023-08-09 12:42:42 -0400

    Chapter 14: Using Dependency Injection

---
+++ ch14/Platform/LocationMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Platform
+{
+	public class LocationMiddleware
+	{
+		private RequestDelegate next;
+		private MessageOptions options;
+
+		public LocationMiddleware(RequestDelegate nextDelegate, IOptions<MessageOptions> opts)
+		{
+			next = nextDelegate;
+			options = opts.Value;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			if (context.Request.Path == "/location" )
+			{
+				await context.Response.WriteAsync($"{options.CityName}, {options.CountryName}");
+			}
+			else
+			{
+				await next(context);
+			}
+		}
+	}
+}
