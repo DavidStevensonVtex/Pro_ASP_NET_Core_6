aaeb06acd45fa2fc23be3a27a6f06af6526a3616
David Stevenson <drstevenson1958@yahoo.com>
2023-08-09 14:41:09 -0400

    Listing 14-20. Resolving Dependencies in the EndpointExtensions.cs file in the Services folder

--- ch13/Platform/Services/EndpointExtensions.cs
+++ ch13/Platform/Services/EndpointExtensions.cs
@@ -1,11 +1,19 @@
-﻿namespace Platform.Services
+﻿using System.Reflection;
+
+namespace Microsoft.AspNetCore.Builder
 {
 	public static class EndpointExtensions
 	{
-		public static void MapWeather(this IEndpointRouteBuilder app, string path)
+		public static void MapEndpoint<T>(this IEndpointRouteBuilder app, string path, string methodName = "Endpoint")
 		{
-			IResponseFormatter formatter = app.ServiceProvider.GetRequiredService<IResponseFormatter>();
-			app.MapGet(path, context => Platform.WeatherEndpoint.Endpoint(context, formatter));
+			MethodInfo? methodInfo = typeof(T).GetMethod(methodName);
+			if (methodInfo == null || methodInfo.ReturnType != typeof(Task))
+			{
+				throw new System.Exception("Method cannot be used");
+			}
+
+			T endpointInstance = ActivatorUtilities.CreateInstance<T>(app.ServiceProvider);
+			app.MapGet(path, (RequestDelegate)methodInfo.CreateDelegate(typeof(RequestDelegate), endpointInstance));
 		}
 	}
 }
