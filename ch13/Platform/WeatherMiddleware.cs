f5442bfd5fe100a0fe13c9c79dd477d2a077bf3e
David Stevenson <drstevenson1958@yahoo.com>
2023-08-09 14:03:40 -0400

    Listing 14-14. Declaring a Dependency in the WeatherMiddleware.cs file in the Services folder

--- ch13/Platform/WeatherMiddleware.cs
+++ ch13/Platform/WeatherMiddleware.cs
@@ -1,19 +1,23 @@
-﻿namespace Platform
+﻿using Platform.Services;
+
+namespace Platform
 {
 	public class WeatherMiddleware
 	{
 		private RequestDelegate next;
+		private IResponseFormatter formatter;
 
-		public WeatherMiddleware(RequestDelegate nextDelegate)
+		public WeatherMiddleware(RequestDelegate nextDelegate, IResponseFormatter respFormatter)
 		{
 			next = nextDelegate;
+			formatter = respFormatter;
 		}
 
 		public async Task Invoke(HttpContext context)
 		{
 			if (context.Request.Path == "/middleware/class")
 			{
-				await context.Response.WriteAsync("Middleware Class: It is raining in London");
+				await formatter.Format(context, "Middleware Class: It is raining in London");
 			}
 			else
 			{
