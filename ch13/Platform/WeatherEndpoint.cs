12ef49530485c8da9ee9d63dd9817ce2fee4f685
David Stevenson <drstevenson1958@yahoo.com>
2023-08-09 14:11:45 -0400

    Listing 14-15. Using a Service in the WeatherEndpoint.cs file in the Platform folder

--- ch13/Platform/WeatherEndpoint.cs
+++ ch13/Platform/WeatherEndpoint.cs
@@ -1,10 +1,14 @@
-﻿namespace Platform
+﻿using Platform.Services;
+
+namespace Platform
 {
 	public class WeatherEndpoint 
 	{
 		public static async Task Endpoint(HttpContext context)
 		{
-			await context.Response.WriteAsync("Endpoint Class: It is cloudy in Milan");
+			IResponseFormatter formatter = context.RequestServices.GetRequiredService<IResponseFormatter>();
+
+			await formatter.Format(context, "Endpoint Class: It is cloudy in Milan");
 		}
 	}
 }
