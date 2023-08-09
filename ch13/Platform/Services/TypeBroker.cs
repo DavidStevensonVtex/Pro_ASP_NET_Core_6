f49ec1fdc899f5c9b5308bddd0de9a563f153fb4
David Stevenson <drstevenson1958@yahoo.com>
2023-08-09 13:36:49 -0400

    Listing 14-9. The Contents of the TypeBroker.cs File in the Services folder

---
+++ ch13/Platform/Services/TypeBroker.cs
@@ -0,0 +1,9 @@
+﻿namespace Platform.Services
+{
+	public class TypeBroker
+	{
+		private static IResponseFormatter formatter = new TextResponseFormatter();
+
+		public static IResponseFormatter Formatter => formatter;
+	}
+}
