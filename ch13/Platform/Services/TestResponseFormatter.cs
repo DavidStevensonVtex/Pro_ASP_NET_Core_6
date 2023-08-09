9e1dd4951e301574cab3bec8ffc2e93734281e6c
David Stevenson <drstevenson1958@yahoo.com>
2023-08-09 12:59:27 -0400

    Listing 14-2. The contents of the TextResponseFormatter.cs file in the Services folder

---
+++ ch13/Platform/Services/TestResponseFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Platform.Services
+{
+	public class TestResponseFormatter : IResponseFormatter
+	{
+		private int responseCounter = 0;
+
+		public async Task Format(HttpContext context, string content)
+		{
+			await context.Response.WriteAsync($"Response {++responseCounter}:\n{content}");
+		}
+	}
+}
