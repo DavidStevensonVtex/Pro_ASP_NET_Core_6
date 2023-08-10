aac9f2c6f157f456574415bd7b8f1043d84c6a0d
David Stevenson <drstevenson1958@yahoo.com>
2023-08-10 16:37:25 -0400

    Listing 14-25. Requesting Services in the EndpointExtensions.cs file in the Services folder

--- ch13/Platform/Services/EndpointExtensions.cs
+++ ch13/Platform/Services/EndpointExtensions.cs
@@ -13,7 +13,14 @@ namespace Microsoft.AspNetCore.Builder
 			}
 
 			T endpointInstance = ActivatorUtilities.CreateInstance<T>(app.ServiceProvider);
-			app.MapGet(path, (RequestDelegate)methodInfo.CreateDelegate(typeof(RequestDelegate), endpointInstance));
+
+			ParameterInfo[] methodParams = methodInfo!.GetParameters();
+
+			app.MapGet(path, context => 
+				(Task) (methodInfo.Invoke(
+					endpointInstance, 
+					methodParams.Select(p => p.ParameterType == typeof(HttpContext) ? context :
+					app.ServiceProvider.GetService(p.ParameterType)).ToArray()))!);
 		}
 	}
 }
