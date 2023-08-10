d4b921c008ac10457b95ca8c1ae7a7460120b248
David Stevenson <drstevenson1958@yahoo.com>
2023-08-10 16:45:54 -0400

    Listing 14-27. Adding Dependencie in the WeatherMiddleware.cs file in the Platform folder

--- ch13/Platform/WeatherMiddleware.cs
+++ ch13/Platform/WeatherMiddleware.cs
@@ -5,19 +5,22 @@ namespace Platform
 	public class WeatherMiddleware
 	{
 		private RequestDelegate next;
-		//private IResponseFormatter formatter;
 
 		public WeatherMiddleware(RequestDelegate nextDelegate, IResponseFormatter respFormatter)
 		{
 			next = nextDelegate;
-			//formatter = respFormatter;
 		}
 
-		public async Task Invoke(HttpContext context, IResponseFormatter formatter)
+		public async Task Invoke(HttpContext context,
+			IResponseFormatter formatter1,
+			IResponseFormatter formatter2,
+			IResponseFormatter formatter3)
 		{
 			if (context.Request.Path == "/middleware/class")
 			{
-				await formatter.Format(context, "Middleware Class: It is raining in London");
+				await formatter1.Format(context, string.Empty);
+				await formatter2.Format(context, string.Empty);
+				await formatter3.Format(context, string.Empty);
 			}
 			else
 			{
