b1e7f540618d4fa53542c0c153eab32278e357f9
David Stevenson <drstevenson1958@yahoo.com>
2023-08-10 17:30:46 -0400

    Listing 14-32. The Contents of the TimeStamping.cs file in the Services folder

---
+++ ch13/Platform/Services/TimeStamping.cs
@@ -0,0 +1,14 @@
+﻿namespace Platform.Services
+{
+	public interface ITimeStamper
+	{
+		string TimeStamp { get; set; }
+	}
+	public class TimeStamping
+	{
+		public string TimeStamp
+		{
+			get => DateTime.Now.ToShortTimeString();
+		}
+	}
+}
