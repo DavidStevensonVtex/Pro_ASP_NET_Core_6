1aca4946d93d3c7d61816b18ad1e509c51d18bbd
David Stevenson <drstevenson1958@yahoo.com>
2023-08-10 22:31:11 -0400

    Listing 14-35. Accessing Configuration Data in the Program.cs file in the Platform folder

--- ch13/Platform/Program.cs
+++ ch13/Platform/Program.cs
@@ -8,8 +8,17 @@ namespace Platform
         {
             var builder = WebApplication.CreateBuilder(args);
 
-			builder.Services.AddScoped<IResponseFormatter, TimeResponseFormatter>();
-			builder.Services.AddScoped<ITimeStamper, DefaultTimeStamper>();
+            IWebHostEnvironment env = builder.Environment;
+            if (env.IsDevelopment())
+            {
+				builder.Services.AddScoped<IResponseFormatter, TimeResponseFormatter>();
+				builder.Services.AddScoped<ITimeStamper, DefaultTimeStamper>();
+			}
+            else
+            {
+				builder.Services.AddScoped<IResponseFormatter, HtmlResponseFormatter>();
+			}
+
 
 			var app = builder.Build();
 
