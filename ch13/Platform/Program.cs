1d9cd6f4dafc11a39fc078cc2bae2f2dd10c60ba
David Stevenson <drstevenson1958@yahoo.com>
2023-08-10 23:04:39 -0400

    Listing 14-41. Using an Unbound Type in the Program.cs file in the Platform builder

--- ch13/Platform/Program.cs
+++ ch13/Platform/Program.cs
@@ -8,24 +8,28 @@ namespace Platform
         {
             var builder = WebApplication.CreateBuilder(args);
 
-			builder.Services.AddScoped<IResponseFormatter, TextResponseFormatter>();
-			builder.Services.AddScoped<IResponseFormatter, HtmlResponseFormatter>();
-			builder.Services.AddScoped<IResponseFormatter, GuidService>();
+			builder.Services.AddSingleton(typeof(ICollection<>), typeof(List<>));
 
 			var app = builder.Build();
 
-			app.MapGet("single", async context =>
-			{
-				IResponseFormatter formatter = context.RequestServices
-					.GetRequiredService<IResponseFormatter>();
-				await formatter.Format(context, "Single Service");
-			});
+            app.MapGet("string", async context =>
+            {
+                ICollection<string> collection = context.RequestServices.GetRequiredService<ICollection<string>>();
+                collection.Add($"Request: {DateTime.Now.ToLongTimeString()}\n");
+                foreach (string str in collection)
+                {
+                    await context.Response.WriteAsync($"String: {str}");
+                }
+            });
 
-			app.MapGet("/", async context =>
+			app.MapGet("int", async context =>
 			{
-				IResponseFormatter formatter = context.RequestServices
-					.GetServices<IResponseFormatter>().First(f => f.RichOutput);
-				await formatter.Format(context, "Multiple services");
+				ICollection<int> collection = context.RequestServices.GetRequiredService<ICollection<int>>();
+				collection.Add(collection.Count() + 1);
+				foreach (int val in collection)
+				{
+					await context.Response.WriteAsync($"Int: {val}\n");
+				}
 			});
 
 			app.Run();
