6ad0a2de21a306c27105ca2dabcada271f082c87
David Stevenson <drstevenson1958@yahoo.com>
2023-08-11 13:31:42 -0400

    Listing 15-19. Configuring the Application in the Program.cs file in the Platform folder

--- ch15/Platform/Program.cs
+++ ch15/Platform/Program.cs
@@ -6,38 +6,9 @@ namespace Platform
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            var servicesConfig = builder.Configuration;
-            builder.Services.Configure<MessageOptions>(servicesConfig.GetSection("Location"));
-
-            var servicesEnv = builder.Environment;
-            // - use environment to set up services
-
 			var app = builder.Build();
 
-            var pipelineConfig = app.Configuration;
-            // - use configuration settings to set up pipeline
-
-            var pipelineEnv = app.Environment;
-            // - use environment to set up pipeline
-
-            app.UseMiddleware<LocationMiddleware>();
-
-            app.MapGet("config", async (HttpContext context, IConfiguration config, IWebHostEnvironment env) =>
-            {
-                string defaultDebug = config["Logging:LogLevel:Default"];
-                await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
-                await context.Response.WriteAsync($"\nThe env setting is: {env.EnvironmentName}");
-
-                string wsID = config["WebService:Id"];
-                string wsKey = config["WebService:Key"];
-                await context.Response.WriteAsync($"\nThe secret ID is: {wsID}");
-                await context.Response.WriteAsync($"\nThe secret Key is: {wsKey}");
-            });
-
-            app.MapGet("/", async context =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.MapGet("population/{city?}", Population.Endpoint);
 
 			app.Run();
         }
