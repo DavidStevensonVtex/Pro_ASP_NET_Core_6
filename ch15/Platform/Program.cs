30b1919a6a3d93eed4579d5029a10bc69cda354d
David Stevenson <drstevenson1958@yahoo.com>
2023-08-11 12:04:32 -0400

    Listing 15-8. Using Configuration Data in the Program.cs file in the Platform folder

--- ch15/Platform/Program.cs
+++ ch15/Platform/Program.cs
@@ -7,11 +7,14 @@ namespace Platform
             var builder = WebApplication.CreateBuilder(args);
 
             var servicesConfig = builder.Configuration;
-			// - use configuration settings to set up services
+            builder.Services.Configure<MessageOptions>(servicesConfig.GetSection("Location"));
 
 			var app = builder.Build();
+
             var pipelineConfig = app.Configuration;
             // - use configuration settings to set up pipeline
+
+            app.UseMiddleware<LocationMiddleware>();
 
             app.MapGet("config", async (HttpContext context, IConfiguration config) =>
             {
