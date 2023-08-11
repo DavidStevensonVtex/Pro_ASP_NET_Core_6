17a29f8fa391de20757fb1f6d638ef237beb6ad9
David Stevenson <drstevenson1958@yahoo.com>
2023-08-11 13:51:09 -0400

    Listing 15-21. Logging in the Program.cs file in the Platform folder

--- ch15/Platform/Program.cs
+++ ch15/Platform/Program.cs
@@ -8,7 +8,11 @@ namespace Platform
 
 			var app = builder.Build();
 
+            app.Logger.LogDebug("Pipeline configuration starting");
+
             app.MapGet("population/{city?}", Population.Endpoint);
+
+			app.Logger.LogDebug("Pipeline configuration complete");
 
 			app.Run();
         }
