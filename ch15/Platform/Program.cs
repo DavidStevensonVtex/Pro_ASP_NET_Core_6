eb9c86c707c109fd126a9ae0f65ac2d70b7d6cc4
David Stevenson <drstevenson1958@yahoo.com>
2023-08-11 11:38:15 -0400

    Listing 15-4. Accessing Configuration Data in the Program.cs file in the Platform folder

--- ch15/Platform/Program.cs
+++ ch15/Platform/Program.cs
@@ -8,6 +8,12 @@ namespace Platform
 
             var app = builder.Build();
 
+            app.MapGet("config", async (HttpContext context, IConfiguration config) =>
+            {
+                string defaultDebug = config["Logging:LogLevel:Default"];
+                await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
+            });
+
             app.MapGet("/", async context =>
             {
                 await context.Response.WriteAsync("Hello World!");
