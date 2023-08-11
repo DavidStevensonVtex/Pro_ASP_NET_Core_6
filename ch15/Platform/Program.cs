0e4a24a4fc145cd2fc4615aef167b83eea1b7569
David Stevenson <drstevenson1958@yahoo.com>
2023-08-11 11:17:08 -0400

    Listing 15-1. Removing Middleware and Services in the Program.cs file in the Platform folder

--- ch15/Platform/Program.cs
+++ ch15/Platform/Program.cs
@@ -6,42 +6,11 @@ namespace Platform
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.Configure<RouteOptions>(opts =>
-            {
-                opts.ConstraintMap.Add("countryName", typeof(CountryRouteConstraint));
-            });
-
             var app = builder.Build();
 
-            app.Use(async (context, next) =>
+            app.MapGet("/", async context =>
             {
-                Endpoint? end = context.GetEndpoint();
-                if (end != null)
-                {
-                    await context.Response.WriteAsync($"{end.DisplayName} Selected \n");
-                }
-                else
-                {
-                    await context.Response.WriteAsync("No Endpoint Selected \n");
-                }
-                await next();
-            });
-
-            app.Map("{number:int}", async context =>
-            {
-                await context.Response.WriteAsync("Routed to the int endpoint");
-            }).WithDisplayName("Int Endpoint")
-                .Add(b => ((RouteEndpointBuilder)b).Order = 1);
-
-            app.Map("{number:double}", async context =>
-            {
-                await context.Response.WriteAsync("Routed to the double endpoint");
-			}).WithDisplayName("Double Endpoint")
-				.Add(b => ((RouteEndpointBuilder)b).Order = 2);
-
-			app.MapFallback(async context =>
-            {
-                await context.Response.WriteAsync("Routed to fallback endpoint");
+                await context.Response.WriteAsync("Hello World!");
             });
 
 			app.Run();
