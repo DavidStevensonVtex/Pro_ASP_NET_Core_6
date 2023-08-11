3be52304e1dc57d7aaa46763dae4889a301e1f15
David Stevenson <drstevenson1958@yahoo.com>
2023-08-11 13:55:39 -0400

    Listing 15-22. Creating a Logger in the Program.cs file in the Platform folder

--- ch15/Platform/Program.cs
+++ ch15/Platform/Program.cs
@@ -8,11 +8,13 @@ namespace Platform
 
 			var app = builder.Build();
 
-            app.Logger.LogDebug("Pipeline configuration starting");
+            var logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+
+			logger.LogDebug("Pipeline configuration starting");
 
             app.MapGet("population/{city?}", Population.Endpoint);
 
-			app.Logger.LogDebug("Pipeline configuration complete");
+			logger.LogDebug("Pipeline configuration complete");
 
 			app.Run();
         }
