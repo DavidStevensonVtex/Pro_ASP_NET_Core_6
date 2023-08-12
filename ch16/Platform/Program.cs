f6010569887e48beb042095f7cf6d25626f5b8d6
David Stevenson <drstevenson1958@yahoo.com>
2023-08-12 16:39:14 -0400

    Listing 16-19. Adding Status Code Middleware in the Program.cs file in the Platform folder

--- ch16/Platform/Program.cs
+++ ch16/Platform/Program.cs
@@ -17,6 +17,21 @@ namespace Platform
                 app.UseStaticFiles();
             }
 
+            app.UseStatusCodePages("text/html", Platform.Responses.DefaultResponse);
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/error")
+                {
+                    context.Response.StatusCode = StatusCodes.Status404NotFound;
+                    await Task.CompletedTask;
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
             app.Run(context =>
             {
                 throw new Exception("Something has gone wrong");
