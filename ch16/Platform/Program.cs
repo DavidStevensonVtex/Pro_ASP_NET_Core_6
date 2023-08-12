17f94d35203f55bd7ca7aa8816ae6858cf9af41f
David Stevenson <drstevenson1958@yahoo.com>
2023-08-12 16:19:20 -0400

    Listing 16-14. Adding a Middleware Component in the Program.cs file in the Platform folder

--- ch16/Platform/Program.cs
+++ ch16/Platform/Program.cs
@@ -9,46 +9,11 @@ namespace Platform
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddDistributedMemoryCache();
-
-            builder.Services.AddSession(opts =>
-            {
-                opts.IdleTimeout = TimeSpan.FromMinutes(30);
-                opts.Cookie.IsEssential = true;
-            });
-
-            builder.Services.AddHsts(opts =>
-            {
-                opts.MaxAge = TimeSpan.FromDays(1);
-                opts.IncludeSubDomains = true;
-            });
-
             var app = builder.Build();
 
-            if (app.Environment.IsProduction())
+            app.Run(context =>
             {
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseSession();
-
-            app.UseMiddleware<ConsentMiddleware>();
-
-            app.MapGet("/session", async context =>
-            {
-                int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
-                int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
-                context.Session.SetInt32("counter1", counter1);
-                context.Session.SetInt32("counter2", counter2);
-                await context.Session.CommitAsync();
-                await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-            });
-
-            app.MapFallback(async context =>
-            {
-                await context.Response.WriteAsync($"HTTPS Request: {context.Request.IsHttps}\n");
-                await context.Response.WriteAsync("Hello World!");
+                throw new Exception("Something has gone wrong");
             });
 
 			app.Run();
