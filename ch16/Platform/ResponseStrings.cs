4e150525473fed2cc2769d28fba8be2c6ba6d940
David Stevenson <drstevenson1958@yahoo.com>
2023-08-12 16:33:18 -0400

    Listing 16-18. The contents of the ResponseStrings.cs file in the Platform folder

---
+++ ch16/Platform/ResponseStrings.cs
@@ -0,0 +1,19 @@
+﻿namespace Platform
+{
+	public class Responses
+	{
+		public static string DefaultResponse = @"
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset=""utf-8"" />
+                <title>Error</title>
+                <link href=""lib/bootstrap/dist/css/bootstrap.min.css"" rel=""stylesheet"" />
+            </head>
+            <body>
+                <h3 class=""p-2"">Error {0}</h3>
+                <h6>You can go back to the <a href=""/"">homepage</a> and try again.</h6>
+            </body>
+            </html>";
+	}
+}
