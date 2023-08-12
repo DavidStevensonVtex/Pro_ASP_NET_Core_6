04badae6094755e77dc76a5944abeef252f3d72c
David Stevenson <drstevenson1958@yahoo.com>
2023-08-12 13:42:33 -0400

    Listing 16-1. Replacing the contents of the Program.cs file in the Platform folder

--- ch16/Platform/Program.cs
+++ ch16/Platform/Program.cs
@@ -9,25 +9,9 @@ namespace Platform
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddHttpLogging(opts =>
-            {
-                opts.LoggingFields = HttpLoggingFields.RequestMethod |
-                    HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode;
-            });
-
             var app = builder.Build();
 
-            app.UseHttpLogging();
-
-            app.UseStaticFiles();
-
-            var env = app.Environment;
-            app.UseStaticFiles(new StaticFileOptions {
-                FileProvider = new PhysicalFileProvider ($"{env.ContentRootPath}/staticfiles"),
-                RequestPath = "/files"
-            });
-
-            app.MapGet("population/{city?}", Population.Endpoint);
+            app.MapFallback(async context => await context.Response.WriteAsync("Hello World!"));
 
 			app.Run();
         }
