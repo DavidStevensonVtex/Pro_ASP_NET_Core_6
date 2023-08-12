d371230a2778d72225987cb647bacef9fa13136d
David Stevenson <drstevenson1958@yahoo.com>
2023-08-12 15:26:10 -0400

    Listing 16-7. Configuring Sessions in the Program.cs file in the Platform folder

--- ch16/Platform/Program.cs
+++ ch16/Platform/Program.cs
@@ -9,43 +9,19 @@ namespace Platform
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.Configure<CookiePolicyOptions>(opts =>
+            builder.Services.AddDistributedMemoryCache();
+
+            builder.Services.AddSession(opts =>
             {
-                opts.CheckConsentNeeded = context => true;
+                opts.IdleTimeout = TimeSpan.FromMinutes(30);
+                opts.Cookie.IsEssential = true;
             });
 
             var app = builder.Build();
 
-            app.UseCookiePolicy();
+            app.UseSession();
+
             app.UseMiddleware<ConsentMiddleware>();
-
-            app.MapGet("/cookie", async context =>
-            {
-                int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
-                context.Response.Cookies.Append("counter1", counter1.ToString(),
-                    new CookieOptions
-                    {
-                        MaxAge = TimeSpan.FromMinutes(30),
-                        IsEssential = true
-                    });
-
-                int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
-                context.Response.Cookies.Append("counter2", counter2.ToString(),
-                    new CookieOptions
-                    {
-                        MaxAge = TimeSpan.FromMinutes(30)
-                    });
-
-                await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-            });
-
-            app.MapGet("clear", context =>
-            {
-                context.Response.Cookies.Delete("counter1");
-                context.Response.Cookies.Delete("counter2");
-                context.Response.Redirect("/");
-                return Task.CompletedTask;
-            });
 
             app.MapFallback(async context => await context.Response.WriteAsync("Hello World!"));
 
