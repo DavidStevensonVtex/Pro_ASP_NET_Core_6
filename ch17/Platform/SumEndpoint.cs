b8ba696be513d3d98eb89d09ce52a76fc18f8c1b
David Stevenson <drstevenson1958@yahoo.com>
2023-08-14 10:15:26 -0400

    Listing 17-3. The contents of the SumEndpoint.cs file in the Platform folder

---
+++ ch17/Platform/SumEndpoint.cs
@@ -0,0 +1,19 @@
+﻿namespace Platform
+{
+	public class SumEndpoint
+	{
+		public async Task Endpoint(HttpContext context)
+		{
+			int count;
+			int.TryParse((string?)context.Request.RouteValues["count"], out count);
+			long total = 0;
+			for (int i = 1; i <= count; i++ )
+			{
+				total += i;
+			}
+			string totalString = $"({DateTime.Now.ToLongTimeString()}) {total}";
+			await context.Response.WriteAsync($"({DateTime.Now.ToLongTimeString()}) Total for {count}" +
+				$" values: \n{ totalString}\n");
+		}
+	}
+}
