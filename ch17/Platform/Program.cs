7a9815c4e7a04c554a4dd49d050623e94996f901
David Stevenson <drstevenson1958@yahoo.com>
2023-08-14 10:11:25 -0400

    Listing 17-1. Replacing the Contents of the Program.cs File in the Platform folder

--- ch17/Platform/Program.cs
+++ ch17/Platform/Program.cs
@@ -8,38 +8,11 @@ namespace Platform
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.Configure<HostFilteringOptions>(opts =>
-            {
-                opts.AllowedHosts.Clear();
-                opts.AllowedHosts.Add("*.example.com");
-            });
-
             var app = builder.Build();
 
-            if (!app.Environment.IsDevelopment())
+            app.MapGet("/", async context =>
             {
-                app.UseExceptionHandler("/error.html");
-                app.UseStaticFiles();
-            }
-
-            app.UseStatusCodePages("text/html", Platform.Responses.DefaultResponse);
-
-            app.Use(async (context, next) =>
-            {
-                if (context.Request.Path == "/error")
-                {
-                    context.Response.StatusCode = StatusCodes.Status404NotFound;
-                    await Task.CompletedTask;
-                }
-                else
-                {
-                    await next();
-                }
-            });
-
-            app.Run(context =>
-            {
-                throw new Exception("Something has gone wrong");
+                await context.Response.WriteAsync("Hello World!");
             });
 
 			app.Run();
