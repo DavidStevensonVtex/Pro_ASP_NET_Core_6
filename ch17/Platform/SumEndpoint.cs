200a248c66d64fbfc7012ebdf4c9aae08b40e4b9
David Stevenson <drstevenson1958@yahoo.com>
2023-08-14 11:00:15 -0400

    Listing 17-5.  Using the CAche Service in the sumEndpoint.cs file in the Platform folder

--- ch17/Platform/SumEndpoint.cs
+++ ch17/Platform/SumEndpoint.cs
@@ -1,17 +1,30 @@
-﻿namespace Platform
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace Platform
 {
 	public static class SumEndpoint
 	{
-		public static async Task Endpoint(HttpContext context)
+		public static async Task Endpoint(HttpContext context, IDistributedCache cache)
 		{
 			int count;
 			int.TryParse((string?)context.Request.RouteValues["count"], out count);
-			long total = 0;
-			for (int i = 1; i <= count; i++ )
+			string cacheKey = $"sum_{count}";
+			string totalString = await cache.GetStringAsync(cacheKey);
+			if (totalString == null )
 			{
-				total += i;
+				long total = 0;
+				for (int i = 1; i <= count; i++)
+				{
+					total += i;
+				}
+				totalString = $"({DateTime.Now.ToLongTimeString()}) {total}";
+				await cache.SetStringAsync(cacheKey, totalString,
+					new DistributedCacheEntryOptions
+					{
+						AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(2)
+					});
 			}
-			string totalString = $"({DateTime.Now.ToLongTimeString()}) {total}";
+
 			await context.Response.WriteAsync($"({DateTime.Now.ToLongTimeString()}) Total for {count}" +
 				$" values: \n{ totalString}\n");
 		}
