1d0141ee063ffdacba17ce3f0b3fc80304c079d3
David Stevenson <drstevenson1958@yahoo.com>
2023-08-14 12:43:18 -0400

    Listing 17-27. Using a Database in the SumEndpoint.cs File in the Platform folder

--- ch17/Platform/SumEndpoint.cs
+++ ch17/Platform/SumEndpoint.cs
@@ -1,31 +1,30 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
+using Platform.Models;
 using Platform.Services;
 
 namespace Platform
 {
 	public static class SumEndpoint
 	{
-		public static async Task Endpoint(HttpContext context, IDistributedCache cache,
-			IResponseFormatter formatter, LinkGenerator generator)
+		public static async Task Endpoint(HttpContext context, CalculationContext dataContext)
 		{
 			int count;
 			int.TryParse((string?)context.Request.RouteValues["count"], out count);
-
-			long total = 0;
-			for (int i = 1; i <= count; i++)
+			long total = dataContext.Calculations?.FirstOrDefault(c => c.Count == count)?.Result ?? 0;
+			if (total == 0)
 			{
-				total += i;
+				for ( int i = 1; i <= count; i++ )
+				{
+					total += i;
+				}
+
+				dataContext.Calculations?.Add(new() { Count = count, Result = total });
+				await dataContext.SaveChangesAsync();
 			}
+
 			string totalString = $"({DateTime.Now.ToLongTimeString()}) {total}";
-
-			context.Response.Headers["Cache-Control"] = "public, max-age=120";
-
-			//string? url = generator.GetPathByRouteValues(context, null, new { count = count });
-			string? url = $"/sum/{count}";
-
-			await context.Response.WriteAsync($"<div>({DateTime.Now.ToLongTimeString()}) Total for {count}" +
-				$" values: \n{ totalString}</div><div>{totalString}</div>" + 
-				$"<a href={url}>Reload</a>");
+			await context.Response.WriteAsync($"({DateTime.Now.ToLongTimeString()}) Total for {count}" +
+				$" values: \n{ totalString}");
 		}
 	}
 }
