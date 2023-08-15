632cd403f35f2035736d7329dc44567ba80bf79e
David Stevenson <drstevenson1958@yahoo.com>
2023-08-15 14:27:29 -0400

    Listing 19-7. Using Dependency Injection in the ProductsController.cs file in the Controllers folder

--- ch19/WebApp/Controllers/ProductsController.cs
+++ ch19/WebApp/Controllers/ProductsController.cs
@@ -7,20 +7,24 @@ namespace WebApp.Controllers
 	//[ApiController]
 	public class ProductsController : ControllerBase
 	{
+		private DataContext context;
+
+		public ProductsController(DataContext ctx )
+		{
+			context = ctx;
+		}
+
 		[HttpGet]
 		public IEnumerable<Product> GetProducts()
 		{
-			return new Product[]
-			{
-				new Product() { Name = "Product #1" },
-				new Product() { Name = "Product #2" }
-			};
+			return context.Products;
 		}
 
 		[HttpGet("{id}")]
-		public Product GetProduct()
+		public Product? GetProduct([FromServices] ILogger<ProductsController> logger)
 		{
-			return new Product() { ProductId = 1, Name = "Test Product" };
+			logger.LogDebug("GetProduct Action Invoked");
+			return context.Products.FirstOrDefault();
 		}
 	}
 }
