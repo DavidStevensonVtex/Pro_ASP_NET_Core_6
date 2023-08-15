2def4ad299ab2468585de4f1acf7a311c948a5bb
David Stevenson <drstevenson1958@yahoo.com>
2023-08-15 15:53:00 -0400

    Listing 19-24. Applying Attributes in the ProductBindingTarget.cs file in the Models folder

--- ch19/WebApp/Models/ProductBindingTarget.cs
+++ ch19/WebApp/Models/ProductBindingTarget.cs
@@ -1,12 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApp.Models
 {
 	public class ProductBindingTarget
 	{
+		[Required]
 		public string Name { get; set; } = "";
+
+		[Range(1,1000)]
 		public decimal Price { get; set; }
+
+		[Range(1, long.MaxValue)]
 		public long CategoryId { get; set; }
+
+		[Range(1, long.MaxValue)]
 		public long SupplierId { get; set; }
 
 		public Product ToProduct() => new Product()
