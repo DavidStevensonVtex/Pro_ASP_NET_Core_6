d8dbb2f6e4ae4fdefbb5ba716eebed603c59db1f
David Stevenson <drstevenson1958@yahoo.com>
2023-08-15 15:48:49 -0400

    Listing 19-22. Testing Redirection

---
+++ ch19/Listing 19-22. Testing Redirection.ps1
@@ -0,0 +1,9 @@
+Invoke-RestMethod http://localhost:5000/api/products/redirect
+
+# productId  : 1
+# name       : Green Kayak
+# price      : 275.00
+# categoryId : 1
+# category   :
+# supplierId : 1
+# supplier   :
