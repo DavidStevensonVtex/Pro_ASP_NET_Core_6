c98445f065b4ba2610e0958b7ee438979ba6af96
David Stevenson <drstevenson1958@yahoo.com>
2023-08-15 15:45:45 -0400

    Listing 19-21. Redirecting in the ProductsController.cs file in the Controllers folder

--- ch19/WebApp/Controllers/ProductsController.cs
+++ ch19/WebApp/Controllers/ProductsController.cs
@@ -53,5 +53,11 @@ namespace WebApp.Controllers
 			context.Products.Remove(new Product() { ProductId = id });
 			await context.SaveChangesAsync();
 		}
+
+		[HttpGet("redirect")]
+		public IActionResult Redirect()
+		{
+			return Redirect("/api/products/1");
+		}
 	}
 }
