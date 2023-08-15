1941d2563c5446fccc9375c136cf9fa81f1399ab
David Stevenson <drstevenson1958@yahoo.com>
2023-08-15 14:09:02 -0400

    Listing 19-6. The contents of the ProductsController.cs File in the Controllers folder

---
+++ ch19/WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+	[Route("api/[controller]")]
+	//[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<Product> GetProducts()
+		{
+			return new Product[]
+			{
+				new Product() { Name = "Product #1" },
+				new Product() { Name = "Product #2" }
+			};
+		}
+
+		[HttpGet("{id}")]
+		public Product GetProduct()
+		{
+			return new Product() { ProductId = 1, Name = "Test Product" };
+		}
+	}
+}
