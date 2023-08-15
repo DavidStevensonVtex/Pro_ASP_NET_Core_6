e910656b8bec212894d1d6c0de88bce98bd27882
David Stevenson <drstevenson1958@yahoo.com>
2023-08-15 15:12:55 -0400

    Listing 19-16. The contents of the ProductBindingTarget.cs file in the WebApp folder

---
+++ ch19/WebApp/Models/ProductBindingTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
+{
+	public class ProductBindingTarget
+	{
+		public string Name { get; set; } = "";
+		public decimal Price { get; set; }
+		public long CategoryId { get; set; }
+		public long SupplierId { get; set; }
+
+		public Product ToProduct() => new Product()
+		{
+			Name = this.Name,
+			Price = this.Price,
+			CategoryId = this.CategoryId,
+			SupplierId = this.SupplierId
+		};
+	}
+}
