2b8d1d95188f7f92210d054a6fae160bea00ddfb
David Stevenson <drstevenson1958@yahoo.com>
2023-08-15 13:33:52 -0400

    Listing 19-3. Creating a Web Service in the Program.cs File in the Platform folder

--- ch19/WebApp/Program.cs
+++ ch19/WebApp/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 using WebApp.Models;
 
 namespace WebApp
@@ -15,6 +16,43 @@ namespace WebApp
 			});
 
 			var app = builder.Build();
+
+			const string BASEURL = "api/products";
+
+			app.MapGet($"{BASEURL}/{{id}}", async (HttpContext context, DataContext data) =>
+			{
+				string? id = context.Request.RouteValues["id"] as string;
+				if (id != null)
+				{
+					Product? p = data.Products.Find(long.Parse(id));
+					if (p == null)
+					{
+						context.Response.StatusCode = StatusCodes.Status404NotFound;
+					}
+					else
+					{
+						context.Response.ContentType = "application/json";
+						await context.Response.WriteAsync(JsonSerializer.Serialize(p));
+					}
+				}
+			});
+
+			app.MapGet(BASEURL, async (HttpContext context, DataContext data) =>
+			{
+				context.Response.ContentType = "application/json";
+				await context.Response.WriteAsync(JsonSerializer.Serialize(data.Products));
+			});
+
+			app.MapPost(BASEURL, async (HttpContext context, DataContext data) =>
+			{
+				Product? p = await JsonSerializer.DeserializeAsync<Product>(context.Request.Body);
+				if (p != null)
+				{
+					await data.AddAsync(p);
+					await data.SaveChangesAsync();
+					context.Response.StatusCode = StatusCodes.Status200OK;
+				}
+			});
 
 			app.UseMiddleware<TestMiddleware>();
 
