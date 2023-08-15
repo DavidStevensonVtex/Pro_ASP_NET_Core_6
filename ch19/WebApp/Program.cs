520988fcc89192d0742f2472e206e80dc419ae04
David Stevenson <drstevenson1958@yahoo.com>
2023-08-15 13:04:42 -0400

    Chapter 19: Creating RESTful Web Services

---
+++ ch19/WebApp/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddDbContext<DataContext>(opts =>
+			{
+				opts.UseSqlServer(builder.Configuration["ConnectionStrings:ProductConnection"]);
+			});
+
+			var app = builder.Build();
+
+			app.UseMiddleware<TestMiddleware>();
+
+			app.MapGet("/", () => "Hello World!");
+
+			var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
+			SeedData.SeedDatabase(context);
+
+			app.Run();
+		}
+	}
+}
