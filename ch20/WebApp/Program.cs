4e961025613d3adbf7716112631b301206105b51
David Stevenson <drstevenson1958@yahoo.com>
2023-08-16 10:19:34 -0400

    Chapter 20: Advanced Web Service Features

---
+++ ch20/WebApp/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using WebApp.Models;
+
+namespace WebApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddDbContext<DataContext>(opts =>
+			{
+				opts.UseSqlServer(builder.Configuration["ConnectionStrings:ProductConnection"]);
+				opts.EnableSensitiveDataLogging(true);
+			});
+
+			builder.Services.AddControllers();
+
+			builder.Services.Configure<JsonOptions>(opts =>
+			{
+				// Doesn't seem to work. JsonSerializerOptions renamed to SerializerOptions.
+				// https://github.com/dotnet/docs/issues/27824
+				opts.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+			});
+
+			var app = builder.Build();
+
+			app.MapControllers();
+
+			app.MapGet("/", () => "Hello World!");
+
+			var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
+			SeedData.SeedDatabase(context);
+
+			app.Run();
+		}
+	}
+}
