a9c24fd4465a403423351bbfbc36a4a4c5e85542
David Stevenson <drstevenson1958@yahoo.com>
2023-08-16 13:38:02 -0400

    Listing 20-16. Requesting an XML Response with a Fallback

---
+++ ch20/Listing 20-16. Requesting an XML Response with a Fallback.ps1
@@ -0,0 +1,7 @@
+$r = Invoke-WebRequest http://localhost:5000/api/content/object -Headers @{ Accept = "application/xml, */*;q=0.8" }
+# $r
+"Status Code: $($r.StatusCode)   Content-Type: $($r.Headers["Content-Type"])"
+"Content: $($r.Content)"
+
+# Status Code: 200   Content-Type: application/json; charset=utf-8
+# Content: {"name":"Kayak","price":275.00,"categoryId":1,"supplierId":1}
