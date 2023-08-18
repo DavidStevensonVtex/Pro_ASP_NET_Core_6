ab73a38c34cb79d47fe812a4c8388050cde210d2
David Stevenson <drstevenson1958@yahoo.com>
2023-08-18 11:31:25 -0400

    Listing 22-5. Using the View Bag in the HomeController.cs file in the Controllers folder

--- ch22/WebApp/Controllers/HomeController.cs
+++ ch22/WebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -14,6 +15,7 @@ namespace WebApp.Controllers
 
 		public async Task<IActionResult> Index(long id = 1)
 		{
+			ViewBag.AveragePrice = await context.Products.AverageAsync(p => p.Price);
 			return View(await context.Products.FindAsync(id));
 		}
 
