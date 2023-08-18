350be4a024263387dd62f1abad752824d63e7238
David Stevenson <drstevenson1958@yahoo.com>
2023-08-18 11:18:20 -0400

    Chapter 22: Using Controllers with Views, Part II

---
+++ ch22/WebApp/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddDbContext<DataContext>(opts =>
+			{
+				opts.UseSqlServer(builder.Configuration["ConnectionStrings:ProductConnection"]);
+				opts.EnableSensitiveDataLogging(true);
+			});
+
+			builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+			var app = builder.Build();
+
+			app.UseStaticFiles();
+			app.MapControllers();
+			app.MapDefaultControllerRoute();
+
+			var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
+			SeedData.SeedDatabase(context);
+
+			app.Run();
+		}
+	}
+}
