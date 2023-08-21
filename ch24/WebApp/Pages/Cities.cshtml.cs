b0d43a8638765de2fa2d99e444575b444fea41cc
David Stevenson <drstevenson1958@yahoo.com>
2023-08-21 11:40:10 -0400

    Listing 24-29. The contents of the Cities.cshtml.cs file in the Pages folder

--- ch24/WebApp/Pages/Cities.cshtml.cs
+++ ch24/WebApp/Pages/Cities.cshtml.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using WebApp.Models;
 
 namespace WebApp.Pages
 {
+    [ViewComponent(Name = "CitiesPageHybrid")]
     public class CitiesModel : PageModel
     {
-        public void OnGet()
+        public CitiesData? Data { get; set; }
+        public CitiesModel (CitiesData cdata)
         {
+            Data = cdata;
+        }
+
+        [ViewComponentContext]
+        public ViewComponentContext Context { get; set; } = new();
+
+        public IViewComponentResult Invoke()
+        {
+            return new ViewViewComponentResult()
+            {
+                ViewData = new ViewDataDictionary<CityViewModel>(
+                    Context.ViewData,
+                    new CityViewModel
+                    {
+                        Cities = Data?.Cities.Count(),
+                        Population = Data?.Cities.Sum(c => c.Population)
+                    })
+            };
         }
     }
 }
