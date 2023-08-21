db6d86068ffc58d39dfe355b148503022aecbece
David Stevenson <drstevenson1958@yahoo.com>
2023-08-21 10:17:01 -0400

    Listing 24-7. The contents of the CitySummary.cs file in the Components folder

---
+++ ch24/WebApp/Components/CitySummary.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Components
+{
+    public class CitySummary : ViewComponent
+    {
+        private CitiesData data;
+
+        public CitySummary(CitiesData cdata)
+        {
+            data = cdata;
+        }
+
+        public string Invoke()
+        {
+            return $"{data.Cities.Count()} cities, {data.Cities.Sum(c => c.Population)} people";
+        }
+    }
+}
