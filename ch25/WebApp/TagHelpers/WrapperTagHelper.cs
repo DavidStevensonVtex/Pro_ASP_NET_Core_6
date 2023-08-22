175f694214992f6209c78091712570bfc2fa9b38
David Stevenson <drstevenson1958@yahoo.com>
2023-08-22 17:45:02 -0400

    Listing 25-15. The contents of the WrapperTagHelper.cs file in the TagHelpers folder

---
+++ ch25/WebApp/TagHelpers/WrapperTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WebApp.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes = "[wrap=true]")]
+    public class WrapperTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            TagBuilder elem = new TagBuilder("div");
+            elem.Attributes["class"] = "bg-primary text-white p-2 m-2";
+            elem.InnerHtml.AppendHtml("Wrapper");
+
+            output.PreElement.AppendHtml(elem);
+            output.PostElement.AppendHtml(elem);
+        }
+    }
+}
