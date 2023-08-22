9353d1ac3dab420dc8fcce72c42ab8564c65c9ac
David Stevenson <drstevenson1958@yahoo.com>
2023-08-22 16:01:41 -0400

    Listing 25-1. The contents ofthe Program.cs file in the WebApp folder

--- ch25/WebApp/Program.cs
+++ ch25/WebApp/Program.cs
@@ -19,24 +19,11 @@ namespace WebApp
 
 			builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 			builder.Services.AddRazorPages();
-
-			builder.Services.AddDistributedMemoryCache();
-			builder.Services.AddSession(options =>
-			{
-				options.Cookie.IsEssential = true;
-			});
-
-			builder.Services.Configure<RazorPagesOptions>(opts =>
-			{
-				opts.Conventions.AddPageRoute("/Index", "/extra/page/{id:long?}");
-			});
-
 			builder.Services.AddSingleton<CitiesData>();
 
 			var app = builder.Build();
 
 			app.UseStaticFiles();
-			app.UseSession();
 			app.MapControllers();
 			app.MapDefaultControllerRoute();
 			app.MapRazorPages();
