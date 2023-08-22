5e0258fe70faf2ae350229a11924be25ad3a5d31
David Stevenson <drstevenson1958@yahoo.com>
2023-08-22 17:23:26 -0400

    Listing 25-13. The contents of TableHeadTagHelper.cs in the TagHelpers folder

---
+++ ch25/WebApp/TagHelpers/TableHeadTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Drawing;
+
+namespace WebApp.TagHelpers
+{
+    [HtmlTargetElement("tablehead")]
+    public class TableHeadTagHelper : TagHelper
+    {
+        public string BgColor { get; set; } = "light";
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "thead";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("class", $"bg-{BgColor} text-white text-center");
+
+            string content = (await output.GetChildContentAsync()).GetContent();
+            output.Content.SetHtmlContent($"<tr><th colspan='2'>{content}</th></tr>");
+        }
+    }
+}
