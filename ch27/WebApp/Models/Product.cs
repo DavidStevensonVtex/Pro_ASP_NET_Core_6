0758b10876146a9b2d257337e732c10636a08aec
David Stevenson <drstevenson1958@yahoo.com>
2023-08-24 15:59:23 -0400

    Listing 27-18. Applying a Formatting Attribute to the Product.cs file in the Models folder

--- ch27/WebApp/Models/Product.cs
+++ ch27/WebApp/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace WebApp.Models
@@ -8,6 +9,7 @@ namespace WebApp.Models
 		public long ProductId { get; set; }
 		public string Name { get; set; }
 		[Column(TypeName = "decimal(8, 2)")]
+		[DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = true)]
 		public decimal Price { get; set; }
 		public long CategoryId { get; set; }
 		public Category? Category { get; set; }
