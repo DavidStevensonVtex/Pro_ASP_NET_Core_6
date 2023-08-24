50aa08fccc34a82adf15c82ba7de965c4f1508df
David Stevenson <drstevenson1958@yahoo.com>
2023-08-24 16:26:04 -0400

    Listing 27-20. Including Related Data in the FormController.cs file in the Controllers folder

--- ch27/WebApp/Controllers/FormController.cs
+++ ch27/WebApp/Controllers/FormController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -14,7 +15,10 @@ namespace WebApp.Controllers
 
         public async Task<IActionResult> Index(long id = 1)
         {
-            return View("Form", await context.Products.FindAsync(id));
+            return View("Form", await context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .FirstAsync(p => p.ProductId == id));
         }
 
         [HttpPost]
