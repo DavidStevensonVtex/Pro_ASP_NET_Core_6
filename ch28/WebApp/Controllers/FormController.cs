80abf827a50f81921396839d298ee07954a03a5f
David Stevenson <drstevenson1958@yahoo.com>
2023-08-25 16:03:14 -0400

    Listing 28-18. Selectively Bidning Properties in the FormController.cs file in the Controllers folder

--- ch28/WebApp/Controllers/FormController.cs
+++ ch28/WebApp/Controllers/FormController.cs
@@ -27,9 +27,11 @@ namespace WebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult SubmitForm([Bind(Prefix="Category")] Category category)
+        public IActionResult SubmitForm([Bind("Name", "Category")] Product product)
         {
-            TempData["category"] = System.Text.Json.JsonSerializer.Serialize(category);
+            TempData["name"] = product.Name;
+            TempData["price"] = product.Price.ToString();
+            TempData["category name"] = product.Category?.Name;
             return RedirectToAction(nameof(Results));
         }
 
