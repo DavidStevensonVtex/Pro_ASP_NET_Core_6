e5b11655cdeab3d6b46d0082f2a959d5c06d11a7
David Stevenson <drstevenson1958@yahoo.com>
2023-08-26 14:32:29 -0400

    Listing 28-26. Selecting the Query String in the FormControler.cs file in the Controllers folder

--- ch28/WebApp/Controllers/FormController.cs
+++ ch28/WebApp/Controllers/FormController.cs
@@ -16,7 +16,7 @@ namespace WebApp.Controllers
             context = ctx;
         }
 
-        public async Task<IActionResult> Index(long? id)
+        public async Task<IActionResult> Index([FromQuery] long? id)
         {
             ViewBag.Categories = new SelectList(context.Categories, "CategoryId", "Name");
 
