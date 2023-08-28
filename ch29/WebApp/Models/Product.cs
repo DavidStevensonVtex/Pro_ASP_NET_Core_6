72776c4489962a507a2f1632e257d29dabf297e7
David Stevenson <drstevenson1958@yahoo.com>
2023-08-28 17:45:56 -0400

    Listing 29-18. The contents of the PhraseAndPriceAttribute.cs file in the Validation folder

--- ch29/WebApp/Models/Product.cs
+++ ch29/WebApp/Models/Product.cs
@@ -2,9 +2,11 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using WebApp.Validation;
 
 namespace WebApp.Models
 {
+	[PhraseAndPrice(Phrase = "Small", Price = "100")]
 	public class Product
 	{
 		public long ProductId { get; set; }
@@ -15,9 +17,13 @@ namespace WebApp.Models
 		[Range(1,999999, ErrorMessage = "Please enter a positive price")]
 		[Column(TypeName = "decimal(8, 2)")]
 		public decimal Price { get; set; }
-		public long CategoryId { get; set; }
+
+        [PrimaryKey(ContextTypes = typeof(DataContext), DataType = typeof(Category))]
+        public long CategoryId { get; set; }
 		public Category? Category { get; set; }
-		public long SupplierId { get; set; }
+
+        [PrimaryKey(ContextTypes = typeof(DataContext), DataType = typeof(Supplier))]
+        public long SupplierId { get; set; }
 
 		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
 		public Supplier? Supplier { get; set; }
