c824c2a0b86cc63ecbb8b2e45e9ecd6d4f9608f5
David Stevenson <drstevenson1958@yahoo.com>
2023-08-28 16:40:55 -0400

    Listing 29-10. Changing a Validation Message in the Program.cs file in the WebApp folder

--- ch29/WebApp/Program.cs
+++ ch29/WebApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp
 {
@@ -24,6 +25,9 @@ namespace WebApp
 			{
 				opts.HeaderName = "X-XSRF-TOKEN";
 			});
+
+			builder.Services.Configure<MvcOptions>(opts => opts.ModelBindingMessageProvider
+				.SetValueMustNotBeNullAccessor(value => "Please enter a value"));
 
             var app = builder.Build();
 
