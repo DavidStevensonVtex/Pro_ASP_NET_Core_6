d61bbe7309301909b14c40b60dba2bde5bfe4d56
David Stevenson <drstevenson1958@yahoo.com>
2023-08-30 14:49:45 -0400

    Listing 30-7. Configuring the Platform in the Program.cs file in the WebApp folder
    Listing 30-8. Dropping the Database

--- ch30/WebApp/Program.cs
+++ ch30/WebApp/Program.cs
@@ -19,36 +19,11 @@ namespace WebApp
 
 			builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 			builder.Services.AddRazorPages();
-			builder.Services.AddSingleton<CitiesData>();
-
-			builder.Services.Configure<AntiforgeryOptions>(opts =>
-			{
-				opts.HeaderName = "X-XSRF-TOKEN";
-			});
-
-			builder.Services.Configure<MvcOptions>(opts => opts.ModelBindingMessageProvider
-				.SetValueMustNotBeNullAccessor(value => "Please enter a value"));
 
             var app = builder.Build();
 
 			app.UseStaticFiles();
-
-			IAntiforgery antiforgery = app.Services.GetRequiredService<IAntiforgery>();
-			app.Use(async (context, next) =>
-			{
-				if (!context.Request.Path.StartsWithSegments("/api"))
-				{
-					string? token = antiforgery.GetAndStoreTokens(context).RequestToken;
-					if (token != null)
-					{
-						context.Response.Cookies.Append("XSRF-TOKEN", token, new CookieOptions { HttpOnly = false });
-					}
-				}
-
-				await next();
-			});
-
-			app.MapControllerRoute("forms", "controllers/{controller=Home}/{action=Index}/{id?}");
+			app.MapDefaultControllerRoute();
 			app.MapRazorPages();
 
 			var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
