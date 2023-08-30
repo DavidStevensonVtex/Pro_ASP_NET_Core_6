24a2a1ffb792dd2f629388433eaca5f9410d97bb
David Stevenson <drstevenson1958@yahoo.com>
2023-08-30 13:51:08 -0400

    Listing 29-25. The contents of the ValidationController.cs file in the Controllers folder

---
+++ ch29/WebApp/Controllers/ValidationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValidationController : ControllerBase
+    {
+        private DataContext dataContext;
+
+        public ValidationController(DataContext context)
+        {
+            dataContext = context;
+        }
+
+        [HttpGet("categorykey")]
+        public bool CategoryKey(string categoryId)
+        {
+            long keyVal;
+            return long.TryParse(categoryId, out keyVal) &&
+                dataContext.Categories.Find(keyVal) != null;
+        }
+
+        [HttpGet("supplierkey")]
+        public bool SupplierKey(string supplierId)
+        {
+            long keyVal;
+            return long.TryParse(supplierId, out keyVal) &&
+                dataContext.Suppliers.Find(keyVal) != null;
+        }
+    }
+}
