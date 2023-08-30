7f3b60ccd96f710a919197c213d23c1b76795c34
David Stevenson <drstevenson1958@yahoo.com>
2023-08-30 15:07:36 -0400

    Listing 30-13. Applying a Filter to all actions in the HomeController.cs file in the Controllers folder

--- ch30/WebApp/Controllers/HomeController.cs
+++ ch30/WebApp/Controllers/HomeController.cs
@@ -2,15 +2,14 @@
 
 namespace WebApp.Controllers
 {
+    [RequireHttps]
     public class HomeController : Controller
     {
-        [RequireHttps]
         public IActionResult Index()
         {
             return View("Message", "This is the Index action on the Home controller");
         }
 
-        [RequireHttps]
         public IActionResult Secure()
         {
             return View("Message", "This is the Secure action on the Home controller");
