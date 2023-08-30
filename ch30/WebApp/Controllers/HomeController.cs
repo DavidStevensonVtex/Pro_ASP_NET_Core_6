d72ceed1bbe6d1e97a657c858ed635753af54b3b
David Stevenson <drstevenson1958@yahoo.com>
2023-08-30 15:02:51 -0400

    Listing 30-12. Applying a Filter in the HomeController.cs file in the Controllers folder

--- ch30/WebApp/Controllers/HomeController.cs
+++ ch30/WebApp/Controllers/HomeController.cs
@@ -4,28 +4,16 @@ namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        [RequireHttps]
         public IActionResult Index()
         {
-            if (Request.IsHttps)
-            {
-                return View("Message", "This is the Index action on the Home controller");
-            }
-            else
-            {
-                return new StatusCodeResult(StatusCodes.Status403Forbidden);
-            }
+            return View("Message", "This is the Index action on the Home controller");
         }
 
+        [RequireHttps]
         public IActionResult Secure()
         {
-            if (Request.IsHttps)
-            {
-                return View("Message", "This is the Secure action on the Home controller");
-            }
-            else
-            {
-                return new StatusCodeResult(StatusCodes.Status403Forbidden);
-            }
+            return View("Message", "This is the Secure action on the Home controller");
         }
     }
 }
