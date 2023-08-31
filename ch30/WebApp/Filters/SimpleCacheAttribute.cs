c8bfcdcea583ed75565ff465a752f35b5d31ad1c
David Stevenson <drstevenson1958@yahoo.com>
2023-08-31 13:01:03 -0400

    Listing 30-18. The contents of the SimpleCacheAttribute.cs file in the Filters folder

---
+++ ch30/WebApp/Filters/SimpleCacheAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApp.Filters
+{
+	public class SimpleCacheAttribute : Attribute, IResourceFilter
+	{
+		private Dictionary<PathString, IActionResult> CachedResponses = new Dictionary<PathString, IActionResult>();
+
+		public void OnResourceExecuting(ResourceExecutingContext context)
+		{
+			PathString path = context.HttpContext.Request.Path;
+
+			if (CachedResponses.ContainsKey(path))
+			{
+				context.Result = CachedResponses[path];
+				CachedResponses.Remove(path);
+			}
+		}
+
+		public void OnResourceExecuted(ResourceExecutedContext context)
+		{
+			if (context.Result != null)
+			{
+				CachedResponses.Add(context.HttpContext.Request.Path, context.Result);
+			}
+		}
+	}
+}
