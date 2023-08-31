c9f8f56e8f24dc4eb6d9c65d432ca8b50ba85608
David Stevenson <drstevenson1958@yahoo.com>
2023-08-31 13:09:48 -0400

    Listing 30-20: Creating an Asynchronous Resource Filter in the SimpleCacheAttribute.cs file in the Filters folder

--- ch30/WebApp/Filters/SimpleCacheAttribute.cs
+++ ch30/WebApp/Filters/SimpleCacheAttribute.cs
@@ -3,27 +3,27 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace WebApp.Filters
 {
-	public class SimpleCacheAttribute : Attribute, IResourceFilter
+	public class SimpleCacheAttribute : Attribute, IAsyncResourceFilter
 	{
 		private Dictionary<PathString, IActionResult> CachedResponses = new Dictionary<PathString, IActionResult>();
 
-		public void OnResourceExecuting(ResourceExecutingContext context)
-		{
-			PathString path = context.HttpContext.Request.Path;
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            PathString path = context.HttpContext.Request.Path;
 
-			if (CachedResponses.ContainsKey(path))
+            if (CachedResponses.ContainsKey(path))
+            {
+                context.Result = CachedResponses[path];
+                CachedResponses.Remove(path);
+            }
+			else
 			{
-				context.Result = CachedResponses[path];
-				CachedResponses.Remove(path);
+				ResourceExecutedContext execContext = await next();
+				if (execContext.Result != null)
+				{
+					CachedResponses.Add(context.HttpContext.Request.Path, execContext.Result);
+				}
 			}
-		}
-
-		public void OnResourceExecuted(ResourceExecutedContext context)
-		{
-			if (context.Result != null)
-			{
-				CachedResponses.Add(context.HttpContext.Request.Path, context.Result);
-			}
-		}
-	}
+        }
+    }
 }
