24d7e644d33a2753e28ae3a0221f58ff97ab6333
David Stevenson <drstevenson1958@yahoo.com>
2023-08-31 14:50:12 -0400

    Listing 30-34. Applying a Filter in the HomeController.cs file in the Controllers folder

--- ch30/WebApp/Controllers/HomeController.cs
+++ ch30/WebApp/Controllers/HomeController.cs
@@ -6,47 +6,13 @@ namespace WebApp.Controllers
 {
     [HttpsOnly]
     [ResultDiagnostics]
+    [GuidResponse]
+    [GuidResponse]
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
             return View("Message", "This is the Index action on the Home controller");
-        }
-
-        public IActionResult Secure()
-        {
-            return View("Message", "This is the Secure action on the Home controller");
-        }
-
-        //[ChangeArg]
-        public IActionResult Messages(string message1, string message2 = "None")
-        {
-            return View("Message", $"{message1}, {message2}");
-        }
-
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            if (context.ActionArguments.ContainsKey("message1"))
-            {
-                context.ActionArguments["message1"] = "New message";
-            }
-        }
-
-        [RangeException]
-        public ViewResult GenerateException(int? id)
-        {
-            if (id == null)
-            {
-                throw new ArgumentNullException(nameof(id));
-            }
-            else if (id > 10)
-            {
-                throw new ArgumentOutOfRangeException(nameof(id));
-            }
-            else
-            {
-                return View("Message", $"The value is{id}");
-            }
         }
     }
 }
