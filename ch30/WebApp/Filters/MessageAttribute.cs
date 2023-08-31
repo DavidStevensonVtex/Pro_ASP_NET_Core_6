d6042d0f77fa27c87e31eb385166074a8b32f55e
David Stevenson <drstevenson1958@yahoo.com>
2023-08-31 15:20:01 -0400

    Listing 30-42. Adding Ordering Support in the MessageAttribute.cs file in the Filters Folder

--- ch30/WebApp/Filters/MessageAttribute.cs
+++ ch30/WebApp/Filters/MessageAttribute.cs
@@ -9,12 +9,14 @@ using Microsoft.VisualBasic;
 namespace WebApp.Filters
 {
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
-    public class MessageAttribute : Attribute, IAsyncAlwaysRunResultFilter
+    public class MessageAttribute : Attribute, IAsyncAlwaysRunResultFilter, IOrderedFilter
     {
         private int counter = 0;
         private string msg;
 
         public MessageAttribute(string message) => msg = message;
+
+        public int Order { get; set; }
 
         public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
         {
