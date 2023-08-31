a23404e9d04341a8fd4e29a02155e5f14d01188d
David Stevenson <drstevenson1958@yahoo.com>
2023-08-31 15:14:29 -0400

    Listing 30-40. Applying a filter in the HomeController.cs file in the Controllers folder

--- ch30/WebApp/Controllers/HomeController.cs
+++ ch30/WebApp/Controllers/HomeController.cs
@@ -4,12 +4,11 @@ using WebApp.Filters;
 
 namespace WebApp.Controllers
 {
-    //[HttpsOnly]
-    [ResultDiagnostics]
-    //[GuidResponse]
-    //[GuidResponse]
+    [Message("This is the controller-scoped filter")]
     public class HomeController : Controller
     {
+        [Message("This is the first action-scoped filter")]
+        [Message("This is the second action-scoped filter")]
         public IActionResult Index()
         {
             return View("Message", "This is the Index action on the Home controller");
