2bf0558e5162a7288af0b7f022d5387666d70fdf
David Stevenson <drstevenson1958@yahoo.com>
2023-08-31 15:22:08 -0400

    Listing 30-43. Setting Filter Order in the HomeController.cs File in the Controllers folder

--- ch30/WebApp/Controllers/HomeController.cs
+++ ch30/WebApp/Controllers/HomeController.cs
@@ -4,11 +4,11 @@ using WebApp.Filters;
 
 namespace WebApp.Controllers
 {
-    [Message("This is the controller-scoped filter")]
+    [Message("This is the controller-scoped filter", Order = 10)]
     public class HomeController : Controller
     {
-        [Message("This is the first action-scoped filter")]
-        [Message("This is the second action-scoped filter")]
+        [Message("This is the first action-scoped filter", Order = 1)]
+        [Message("This is the second action-scoped filter", Order = -1)]
         public IActionResult Index()
         {
             return View("Message", "This is the Index action on the Home controller");
