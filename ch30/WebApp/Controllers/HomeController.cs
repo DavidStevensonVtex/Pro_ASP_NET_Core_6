fb0f7d9dc14854b821ef6a793637ae8129e3b6bb
David Stevenson <drstevenson1958@yahoo.com>
2023-08-31 13:24:46 -0400

    Listing 30-22. Applying a Filter in the HomeController.cs file in the Controllers folder

--- ch30/WebApp/Controllers/HomeController.cs
+++ ch30/WebApp/Controllers/HomeController.cs
@@ -15,5 +15,11 @@ namespace WebApp.Controllers
         {
             return View("Message", "This is the Secure action on the Home controller");
         }
+
+        [ChangeArg]
+        public IActionResult Messages(string message1, string message2 = "None" )
+        {
+            return View("Message", $"{message1}, {message2}");
+        }
     }
 }
