006cc284efdf1c24cc7fdac19acedf18e2172cb5
David Stevenson <drstevenson1958@yahoo.com>
2023-09-01 13:43:53 -0400

    Listing 31-12. Adding Actions in the HomeController.cs file in the Controllers folder

--- ch31/WebApp/Controllers/HomeController.cs
+++ ch31/WebApp/Controllers/HomeController.cs
@@ -31,5 +31,26 @@ namespace WebApp.Controllers
             ProductViewModel model = ViewModelFactory.Details(p);
             return View("ProductEditor", model);
         }
+
+        public IActionResult Create()
+        {
+            return View("ProductEditor", ViewModelFactory.Create(new Product(), Categories, Suppliers));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.ProductId = default;
+                product.Category = default;
+                product.Supplier = default;
+                context.Products.Add(product);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View("ProductEditor", ViewModelFactory.Create(product, Categories, Suppliers));
+        }
     }
 }
