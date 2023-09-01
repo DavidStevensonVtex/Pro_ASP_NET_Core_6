01d2dbe2ee8b2327deff918ac7363cd6f27de40e
David Stevenson <drstevenson1958@yahoo.com>
2023-09-01 14:28:10 -0400

    Listing 31-18 The contents of the _ProductEditor.cshtml file in the Pages folder

---
+++ ch31/WebApp/Pages/_ProductEditor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class _ProductEditorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
