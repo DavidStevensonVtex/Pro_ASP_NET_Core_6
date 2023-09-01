dab506673a1f8fec34d6f39f4e9890176d921c7e
David Stevenson <drstevenson1958@yahoo.com>
2023-09-01 12:44:25 -0400

    Listing 31-3. Changing Validation in the Product.cs file in the Models folder

--- ch31/WebApp/Models/Product.cs
+++ ch31/WebApp/Models/Product.cs
@@ -8,7 +8,7 @@ using Microsoft.CodeAnalysis.Completion;
 
 namespace WebApp.Models
 {
-	[PhraseAndPrice(Phrase = "Small", Price = "100")]
+	//[PhraseAndPrice(Phrase = "Small", Price = "100")]
 	public class Product
 	{
 		public long ProductId { get; set; }
@@ -21,12 +21,12 @@ namespace WebApp.Models
 		public decimal Price { get; set; }
 
         [PrimaryKey(ContextTypes = typeof(DataContext), DataType = typeof(Category))]
-		[Remote("CategoryKey", "Validation", ErrorMessage = "Enter an existing key")]
+		//[Remote("CategoryKey", "Validation", ErrorMessage = "Enter an existing key")]
         public long CategoryId { get; set; }
 		public Category? Category { get; set; }
 
         [PrimaryKey(ContextTypes = typeof(DataContext), DataType = typeof(Supplier))]
-        [Remote("SupplierKey", "Validation", ErrorMessage = "Enter an existing key")]
+        //[Remote("SupplierKey", "Validation", ErrorMessage = "Enter an existing key")]
         public long SupplierId { get; set; }
 
 		[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
