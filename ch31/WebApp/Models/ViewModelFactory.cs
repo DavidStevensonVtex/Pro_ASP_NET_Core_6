4187e61e37fc5c31a25b07f1d573721ae088b9b4
David Stevenson <drstevenson1958@yahoo.com>
2023-09-01 13:10:32 -0400

    Listing 31-9. The contents of the ViewModelFactory.cs file in the Models folder

---
+++ ch31/WebApp/Models/ViewModelFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApp.Models
+{
+    public static class ViewModelFactory
+    {
+        public static ProductViewModel Details ( Product p )
+        {
+            return new ProductViewModel
+            {
+                Product = p, Action = "Details",
+                ReadOnly = true, Theme = "info", ShowAction = false,
+                Categories = p == null || p.Category == null ?
+                    Enumerable.Empty<Category>() :
+                    new List<Category> { p.Category },
+                Suppliers = p == null || p.Supplier == null ?
+                    Enumerable.Empty<Supplier>() :
+                    new List<Supplier> { p.Supplier },
+            };
+        }
+    }
+}
