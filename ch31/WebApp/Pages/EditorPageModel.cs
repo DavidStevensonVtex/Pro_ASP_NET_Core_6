5d7499dc69bad48200f745c9482a12abe03bdba7
David Stevenson <drstevenson1958@yahoo.com>
2023-09-01 14:33:40 -0400

    Listing 31-19. The contents of the EditorPageModel.cs file in the Pages folder

---
+++ ch31/WebApp/Pages/EditorPageModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+
+namespace WebApp.Pages
+{
+    public class EditorPageModel : PageModel
+    {
+        public DataContext DataContext { get; set; }
+        public EditorPageModel(DataContext dbContext)
+        {
+            DataContext = dbContext;
+        }
+
+        public IEnumerable<Category> Categories => DataContext.Categories;
+        public IEnumerable<Supplier> Suppliers => DataContext.Suppliers;
+
+        public ProductViewModel? ViewModel { get; set; }
+    }
+}
