85ac517c0421ef430f9384dffed91dd0fc7b3088
David Stevenson <drstevenson1958@yahoo.com>
2023-09-01 13:13:44 -0400

    Listing 31-10. Adding an action method in the HomeControllers.cs file in the Controllers folder

--- ch31/WebApp/Controllers/HomeController.cs
+++ ch31/WebApp/Controllers/HomeController.cs
@@ -23,5 +23,13 @@ namespace WebApp.Controllers
         {
             return View(context.Products.Include(p => p.Category).Include(p => p.Supplier));
         }
+
+        public async Task<IActionResult> Details ( long id )
+        {
+            Product? p = await context.Products.Include(p => p.Category).Include(p => p.Supplier)
+                .FirstOrDefaultAsync(p => p.ProductId == id) ?? new Product();
+            ProductViewModel model = ViewModelFactory.Details(p);
+            return View("ProductEditor", model);
+        }
     }
 }
