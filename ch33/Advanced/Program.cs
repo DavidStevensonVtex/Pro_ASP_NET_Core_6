dfc870596a4bdcd4500d436e8e92124a0c98e909
David Stevenson <drstevenson1958@yahoo.com>
2023-09-06 13:30:17 -0400

    Chapter 33: Using Blazor Server, Part 1

---
+++ ch33/Advanced/Program.cs
@@ -0,0 +1,37 @@
+using Advanced.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Advanced
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<DataContext>(opts =>
+            {
+                opts.UseSqlServer(builder.Configuration["ConnectionStrings:PeopleConnection"]);
+                opts.EnableSensitiveDataLogging(true);
+            });
+
+            var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World !");
+
+            app.UseStaticFiles();
+
+            app.MapControllers();
+            app.MapControllerRoute("controllers", "controllers/{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
+            SeedData.SeedDatabase(context);
+
+            app.Run();
+        }
+    }
+}
